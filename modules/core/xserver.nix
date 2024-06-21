d0f68c8daa7da241fa150f7ba7441d9538707594
Ahwx <ahwx@ahwx.org>
2024-06-21 13:13:14 +0200

    feat: comply with new nixos settings regarding xorg

--- modules/core/xserver.nix
+++ modules/core/xserver.nix
@@ -1,8 +1,10 @@
 { username, ... }: 
 {
-  services.xserver = {
-    enable = true;
-    xkb.layout = "us";
+  services = {
+    xserver = {
+      enable = true;
+      xkb.layout = "us";
+    };
 
     displayManager.autoLogin = {
       enable = true;
