d9667459d80156dd00ae97e04fe216ee87b6578a
Ahwx <ahwx@ahwx.org>
2024-06-21 20:43:51 +0200

    chore: remove games that i'll never use

--- modules/home/gaming.nix
+++ modules/home/gaming.nix
@@ -6,24 +6,5 @@
     # gamescope
     # winetricks
     # inputs.nix-gaming.packages.${pkgs.system}.wine-ge
-
-    ## Cli games
-    _2048-in-terminal
-    vitetris
-    nethack
-    
-    ## Celeste
-    celeste-classic
-    celeste-classic-pm
-
-    ## Doom
-    # gzdoom
-    crispy-doom
-
-    ## Emulation
-    sameboy
-    snes9x
-    # cemu
-    # dolphin-emu
   ];
 }
