937f9c6d104b97b8ac1c35157e3677a02c39f5ee
Ahwx <ahwx@ahwx.org>
2024-06-21 09:48:00 +0200

    feat: set font and remove unused bindings

--- modules/home/kitty.nix
+++ modules/home/kitty.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: 
+{ ... }: 
 {
   programs.kitty = {
     enable = true;
@@ -7,7 +7,7 @@
     
     font = {
       name = "JetBrainsMono Nerd Font";
-      size = 13;
+      size = 9;
     };
 
     settings = {
@@ -31,12 +31,6 @@
     };
 
     keybindings = {  
-      ## Tabs
-      "alt+1" = "goto_tab 1";
-      "alt+2" = "goto_tab 2";
-      "alt+3" = "goto_tab 3";
-      "alt+4" = "goto_tab 4";
-
       ## Unbind
       "ctrl+shift+left" = "no_op";
       "ctrl+shift+right" = "no_op";
