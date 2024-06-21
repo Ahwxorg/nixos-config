769b739363dda6f9519780b318782f3581743481
Ahwx <ahwx@ahwx.org>
2024-06-21 09:36:34 +0200

    feat: set hostname/dns, disable nix-fw

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -1,13 +1,13 @@
 { pkgs, ... }: 
 {
   networking = {
-    hostName = "nixos";
+    hostName = "sakura";
     networkmanager.enable = true;
-    nameservers = [ "1.1.1.1" ];
+    nameservers = [ "9.9.9.9" ];
     firewall = {
       enable = true;
-      allowedTCPPorts = [ 22 80 443 59010 59011 ];
-      allowedUDPPorts = [ 59010 59011 ];
+      # allowedTCPPorts = [ 22 80 443 59010 59011 ];
+      # allowedUDPPorts = [ 59010 59011 ];
       # allowedUDPPortRanges = [
         # { from = 4000; to = 4007; }
         # { from = 8000; to = 8010; }
