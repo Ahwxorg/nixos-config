6d165e59c67f4066df8f4af1ad63a6bd4bfaa740
Ahwx <ahwx@ahwx.org>
2024-06-21 09:46:03 +0200

    feat: enable/disable some modules, remove unused bindings

--- modules/home/default.desktop.nix
+++ modules/home/default.desktop.nix
@@ -1,7 +1,5 @@
-{inputs, username, host, ...}: {
+{ ...}: {
   imports =
        [(import ./default.nix)]
-    ++ [(import ./rider.nix)]                     # C# JetBrain editor
-    # ++ [ (import ./steam.nix) ]
-    ++ [(import ./unity.nix)];
+    ++ [ (import ./steam.nix) ];
 }
--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,25 +1,14 @@
-{inputs, username, host, ...}: {
+{ ...}: {
   imports =
-       [(import ./aseprite/aseprite.nix)]         # pixel art editor
-    ++ [(import ./audacious/audacious.nix)]       # music player
-    ++ [(import ./bat.nix)]                       # better cat command
-    ++ [(import ./btop.nix)]                      # resouces monitor 
-    ++ [(import ./cava.nix)]                      # audio visualizer
-    ++ [(import ./discord.nix)]                   # discord with catppuccin theme
-    ++ [(import ./floorp/floorp.nix)]             # firefox based browser
-    ++ [(import ./gaming.nix)]                    # packages related to gaming
+       [(import ./bat.nix)]                       # better cat command
     ++ [(import ./git.nix)]                       # version control
     ++ [(import ./gtk.nix)]                       # gtk theme
     ++ [(import ./hyprland)]                      # window manager
     ++ [(import ./kitty.nix)]                     # terminal
     ++ [(import ./mako.nix)]                      # notification deamon
-    ++ [(import ./micro.nix)]                     # nano replacement
     ++ [(import ./nvim.nix)]                      # neovim editor
     ++ [(import ./packages.nix)]                  # other packages
-    ++ [(import ./retroarch.nix)]
-    ++ [(import ./rider.nix)]                     # C# JetBrain editor
     ++ [(import ./scripts/scripts.nix)]           # personal scripts
-    ++ [(import ./starship.nix)]                  # shell prompt
     ++ [(import ./swaylock.nix)]                  # lock screen
     ++ [(import ./vscodium.nix)]                  # vscode forck
     ++ [(import ./waybar)]                        # status bar
