dbb62c055511ce4f7ddf2de2d61e66353ce6bd27
Ahwx <ahwx@ahwx.org>
2024-06-21 20:43:12 +0200

    feat: adds yubikey stuff

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -1,17 +1,18 @@
 { ... }:
 {
   imports =
-       [ (import ./bootloader.nix) ]
-    ++ [ (import ./hardware.nix) ]
-    ++ [ (import ./xserver.nix) ]
-    ++ [ (import ./network.nix) ]
-    ++ [ (import ./pipewire.nix) ]
-    ++ [ (import ./program.nix) ]
-    ++ [ (import ./security.nix) ]
-    ++ [ (import ./services.nix) ]
-    ++ [ (import ./system.nix) ]
-    ++ [ (import ./user.nix) ]
-    ++ [ (import ./bluetooth.nix) ]
-    ++ [ (import ./wayland.nix) ];
+       [(import ./bootloader.nix)]
+    ++ [(import ./hardware.nix)]
+    ++ [(import ./xserver.nix)]
+    ++ [(import ./network.nix)]
+    ++ [(import ./pipewire.nix)]
+    ++ [(import ./program.nix)]
+    ++ [(import ./security.nix)]
+    ++ [(import ./services.nix)]
+    ++ [(import ./system.nix)]
+    ++ [(import ./user.nix)]
+    ++ [(import ./bluetooth.nix)]
+    ++ [(import ./yubikey.nix)]
+    ++ [(import ./wayland.nix)];
     # ++ [ (import ./virtualization.nix) ];
 }
