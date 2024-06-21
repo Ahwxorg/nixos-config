dc69dacd51eceec1714830a261f573e724ffe055
Ahwx <ahwx@ahwx.org>
2024-06-21 09:35:48 +0200

    refactor: make nix code look pretty

--- modules/core/bootloader.nix
+++ modules/core/bootloader.nix
@@ -1,7 +1,11 @@
 { pkgs, ... }:
 {
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.systemd-boot.configurationLimit = 10;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+      systemd-boot.configurationLimit = 10;
+    };
+  };
 }
--- modules/core/hardware.nix
+++ modules/core/hardware.nix
@@ -1,10 +1,12 @@
 { pkgs, ... }:
 {  
-  hardware.opengl.enable = true;
-  hardware.opengl.driSupport = true;
-  hardware.opengl.driSupport32Bit = true;
-  hardware.enableRedistributableFirmware = true;
-  hardware.opengl.extraPackages = with pkgs; [
-    
-  ];
+  hardware = {
+    opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
+      extraPackages = with pkgs; [];
+    };
+    enableRedistributableFirmware = true;
+  };
 }
