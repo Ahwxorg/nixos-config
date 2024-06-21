ea1b76483dbe66fdf935c4319889315b1c289bb7
Ahwx <ahwx@ahwx.org>
2024-06-21 16:33:21 +0200

    feat: comply with new nixos settings regarding opengl

--- modules/core/hardware.nix
+++ modules/core/hardware.nix
@@ -1,10 +1,10 @@
 { pkgs, ... }:
 {  
   hardware = {
-    opengl = {
+    graphics = {
       enable = true;
-      driSupport = true;
-      driSupport32Bit = true;
+      # driSupport = true; # Has no effect anymore, as of 2024-06-21
+      # driSupport32Bit = true;
       extraPackages = with pkgs; [];
     };
     enableRedistributableFirmware = true;
