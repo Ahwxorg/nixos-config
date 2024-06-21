70715c1c5409032e3172b8c8d50508c2c0a43752
Ahwx <ahwx@ahwx.org>
2024-06-21 09:49:09 +0200

    feat: add/remove programs, remove unused parentheses

--- modules/home/packages.nix
+++ modules/home/packages.nix
@@ -1,33 +1,29 @@
 { inputs, pkgs, ... }: 
 {
-  home.packages = (with pkgs; [
-    bitwise                           # cli tool for bit / hex manipulation
+  home.packages = with pkgs; [
+    tmux
+    wdisplays
+    htop
+    firefox
+    nsxiv
     eza                               # ls replacement
-    entr                              # perform action when file change
-    fd                                # find replacement
     file                              # Show file information 
     fzf                               # fuzzy finder
     gtt                               # google translate TUI
     gimp
-    gtrash                            # rm replacement, put deleted files in system trash
-    hexdump
-    jdk17                             # java
     lazygit
     libreoffice
-    cinnamon.nemo-with-extensions     # file manager
-    nitch                             # systhem fetch util
+    xfce.thunar                       # file manager
     nix-prefetch-github
     prismlauncher                     # minecraft launcher
     ripgrep                           # grep replacement
-    soundwireserver                   # pass audio to android phone
-    todo                              # cli todo list
-    toipe                             # typing test in the terminal
-    valgrind                          # c memory analyzer
-    yazi                              # terminal file manager
-    youtube-dl
-    gnome.zenity
-    winetricks
+    yt-dlp
     wineWowPackages.wayland
+    element-desktop-wayland
+    spotify
+    thunderbird
+    neofetch
+    yarn
 
     # C / C++
     gcc
@@ -36,28 +32,24 @@
     # Python
     python3
 
-    bleachbit                         # cache cleaner
-    cmatrix
     gparted                           # partition manager
     ffmpeg
     imv                               # image viewer
     killall
     libnotify
-	  man-pages					            	  # extra man pages
+    man-pages			      # extra man pages
     mpv                               # video player
-    ncdu                              # disk space
     openssl
     pamixer                           # pulseaudio command line mixer
     pavucontrol                       # pulseaudio volume controle (GUI)
     playerctl                         # controller for media players
     wl-clipboard                      # clipboard utils for wayland (wl-copy, wl-paste)
-    cliphist                          # clipboard manager
+    cliphist                          # clipboard history manager
     poweralertd
-    qalculate-gtk                     # calculator
     unzip
     wget
     xdg-utils
     xxd
     inputs.alejandra.defaultPackage.${system}
-  ]);
+  ];
 }
