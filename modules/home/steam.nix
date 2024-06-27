04c198db76032aaf8e0069ac75e19fdddcdb3246
Ahwx <ahwx@ahwx.org>
2024-06-28 01:07:58 +0200

    fix: remove gamemode "feature"

--- modules/home/steam.nix
+++ modules/home/steam.nix
@@ -5,7 +5,6 @@
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = false;
   };
-  programs.gamemode.enable = true;
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
     "steam"
     "steam-original"
