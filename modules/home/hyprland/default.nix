8fed51a37dd31ec41bc5d7d3de12c43cb478c78a
Ahwx <ahwx@ahwx.org>
2024-07-10 20:32:04 +0200

    feat: adds  lockscreen (2)

--- modules/home/hyprland/default.nix
+++ modules/home/hyprland/default.nix
@@ -3,5 +3,6 @@
   imports = [ (import ./hyprland.nix) ]
     ++ [ (import ./config.nix) ]
     ++ [ (import ./variables.nix) ]
+    ++ [ (import ./hyprlock.nix) ]
     ++ [ inputs.hyprland.homeManagerModules.default ];
 }
