019fa62cb71cd7c4328591c6354a28a00b4bc532
Ahwx <ahwx@ahwx.org>
2024-07-15 10:20:12 +0200

    feat: adds `violet` specific pkgs

---
+++ modules/home/packages.violet.nix
@@ -0,0 +1,33 @@
+{ inputs, pkgs, ... }: 
+{
+  home.packages = with pkgs; [
+    jq
+    tmux
+    htop
+    eza
+    file
+    fzf
+    lazygit
+    ripgrep
+    yt-dlp
+    neofetch
+    yarn
+
+    # C / C++
+    gcc
+    gnumake
+
+    # Python
+    python3
+
+    ffmpeg
+    killall
+    libnotify
+    man-pages			      # extra man pages
+    openssl
+    unzip
+    wget
+    xxd
+    inputs.alejandra.defaultPackage.${system}
+  ];
+}
