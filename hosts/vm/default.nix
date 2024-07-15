041cad5e7eaccc62e19fe034ef566e232755978a
Ahwx <ahwx@ahwx.org>
2024-07-15 07:43:36 +0200

    chore: clean up code a little

--- hosts/vm/default.nix
+++ hosts/vm/default.nix
@@ -8,10 +8,14 @@
   # kvm/qemu doesn't use UEFI firmware mode by default.
   # so we force-override the setting here 
   # and configure GRUB instead.
-  boot.loader.systemd-boot.enable = lib.mkForce false;
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/vda";
-  boot.loader.grub.useOSProber = false;
+  boot.loader = {
+    systemd-boot.enable = lib.mkForce false;
+    grub = {
+      enable = true;
+      device = "/dev/vda";
+      useOSProber = false;
+    };
+  };
 
   # allow local remote access to make it easier to toy around with the system
   services.openssh = {
