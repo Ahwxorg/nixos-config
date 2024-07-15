a4d2e9dc853229d173c693cee662aa57798c551d
Ahwx <ahwx@ahwx.org>
2024-07-15 10:20:18 +0200

    feat: adds `violet` specific pkgs (2)

--- modules/home/default.violet.nix
+++ modules/home/default.violet.nix
@@ -3,7 +3,7 @@
        [(import ./bat.nix)]                       # better cat command
     ++ [(import ./git.nix)]                       # version control
     ++ [(import ./nvim.nix)]                      # neovim editor
-    ++ [(import ./packages.nix)]                  # other packages
+    ++ [(import ./packages.violet.nix)]           # other packages
     ++ [(import ./scripts/scripts.nix)]           # personal scripts
     ++ [(import ./zsh.nix)]                       # shell
     ++ [(import ./tmux.nix)];                     # terminal multiplexer
