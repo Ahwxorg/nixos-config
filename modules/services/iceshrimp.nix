7b6df8e07d6efacfab4ba700a5a53969a05f4689
Ahwx <ahwx@ahwx.org>
2024-07-15 13:45:58 +0200

    feat: use `specialArgs` instead for `iceshrimp` module; comment out DB for now

--- modules/services/iceshrimp.nix
+++ modules/services/iceshrimp.nix
@@ -1,12 +1,22 @@
-{ ... }:
+{ config, pkgs, iceshrimp, ... }:
 {
-  services.iceshrimp = {
-    enable = true;
-    settings = { 
-      url = "https://fedi.liv.town"; # The domain your Iceshrimp UI will be served on.
-      settings.db.host = "/run/postgresql"; # omitting this setting causes some configurations to fail
+
+  services = {
+    # redis.servers.iceshrimp = {
+    #   enable = true;
+    #   port = 6380;
+    #   bind = "0.0.0.0";
+    #   settings.protected-mode = "no";
+    # };
+
+    iceshrimp = {
+      enable = true;
+      settings = { 
+        url = "https://fedi.liv.town"; # The domain your Iceshrimp UI will be served on.
+        settings.db.host = "/run/postgresql"; # omitting this setting causes some configurations to fail
+      };
+      dbPasswordFile = /var/iceshrimp/dbPasswordFile;
+      secretConfig = /var/iceshrimp/secretConfig.yml;
     };
-    dbPasswordFile = /var/iceshrimp/dbPasswordFile;
-    secretConfig = /var/iceshrimp/secretConfig.yml;
   };
 }
