c773beb592fba5718709abf7963d38e53e627348
Ahwx <ahwx@ahwx.org>
2024-07-17 00:53:11 +0200

    feat: lets `iceshrimp` configure nginx

--- modules/services/iceshrimp.nix
+++ modules/services/iceshrimp.nix
@@ -5,8 +5,8 @@
     postgresql = {
       enable = true;
       initialScript = pkgs.writeText "synapse-init.sql" ''
-        CREATE ROLE "matrix-synapse" WITH LOGIN PASSWORD 'synapse';
-        CREATE DATABASE "matrix-synapse" WITH OWNER "matrix-synapse"
+        CREATE ROLE "iceshrimp" WITH LOGIN PASSWORD 'uph2reeloo3aeDae4muc';
+        CREATE DATABASE "iceshrimp" WITH OWNER "iceshrimp"
           TEMPLATE template0
           LC_COLLATE = "C"
           LC_CTYPE = "C";
@@ -16,6 +16,7 @@
 
     iceshrimp = {
       enable = true;
+      configureNginx = true;
       settings = { 
         url = "https://fedi.liv.town"; # The domain your Iceshrimp UI will be served on.
         settings.db.host = "/run/postgresql"; # omitting this setting causes some configurations to fail
