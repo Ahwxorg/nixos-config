4bba7bbf2a49e739a6e6afb0d449806e7ab99875
Ahwx <ahwx@ahwx.org>
2024-07-17 01:15:15 +0200

    feat?: try with internal db

--- modules/services/iceshrimp.nix
+++ modules/services/iceshrimp.nix
@@ -2,26 +2,27 @@
 {
 
   services = {
-    postgresql = {
-      enable = true;
-      initialScript = pkgs.writeText "iceshrimp.sql" ''
-        CREATE ROLE "iceshrimp" WITH LOGIN PASSWORD 'uph2reeloo3aeDae4muc';
-        CREATE DATABASE "iceshrimp" WITH OWNER "iceshrimp"
-          TEMPLATE template0
-          LC_COLLATE = "C"
-          LC_CTYPE = "C";
-      '';
-    };
+    # postgresql = {
+    #   enable = true;
+    #   initialScript = pkgs.writeText "iceshrimp.sql" ''
+    #     CREATE ROLE "iceshrimp" WITH LOGIN PASSWORD 'uph2reeloo3aeDae4muc';
+    #     CREATE DATABASE "iceshrimp" WITH OWNER "iceshrimp"
+    #       TEMPLATE template0
+    #       LC_COLLATE = "C"
+    #       LC_CTYPE = "C";
+    #   '';
+    # };
 
 
     iceshrimp = {
       enable = true;
       settings = { 
         configureNginx = true;
+        createDb = true;
         url = "https://fedi.liv.town"; # The domain your Iceshrimp UI will be served on.
-        settings.db.host = "/run/postgresql"; # omitting this setting causes some configurations to fail
+        # settings.db.host = "/run/postgresql"; # omitting this setting causes some configurations to fail
       };
-      dbPasswordFile = /var/iceshrimp/dbPasswordFile;
+      # dbPasswordFile = /var/iceshrimp/dbPasswordFile;
       secretConfig = /var/iceshrimp/secretConfig.yml;
     };
   };
