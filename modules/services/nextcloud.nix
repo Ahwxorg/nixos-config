f52da0714cac9582a1a9263d05b4f14ddfba7079
Ahwx <ahwx@ahwx.org>
2024-07-17 01:39:33 +0200

    feat: adds some options for nextcloud

--- modules/services/nextcloud.nix
+++ modules/services/nextcloud.nix
@@ -4,11 +4,28 @@
     enable = true;                   
     hostName = "cloud.liv.town";
     https = true;
+    configureRedis = true;
+    maxUploadSize = "10G";
 
     virtualHosts.${config.services.nextcloud.hostName} = {
       forceSSL = true;
       enableACME = true;
     };
+
+    extraOptions.enabledPreviewProviders = [
+      "OC\\Preview\\BMP"
+      "OC\\Preview\\GIF"
+      "OC\\Preview\\JPEG"
+      "OC\\Preview\\Krita"
+      "OC\\Preview\\MarkDown"
+      "OC\\Preview\\MP3"
+      "OC\\Preview\\OpenDocument"
+      "OC\\Preview\\PNG"
+      "OC\\Preview\\TXT"
+      "OC\\Preview\\XBitmap"
+      "OC\\Preview\\HEIC"
+    ];
+
   };
   
     
