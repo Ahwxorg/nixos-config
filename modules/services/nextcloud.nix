2093da9d90a5cd68b9ed5741a8ea33ed44e1b198
Ahwx <ahwx@ahwx.org>
2024-07-17 03:01:18 +0200

    fix: nextcloud!

--- modules/services/nextcloud.nix
+++ modules/services/nextcloud.nix
@@ -1,40 +1,59 @@
 { config, ... }:
 {
-
+  security.acme = {
+    acceptTerms = true;
+    preliminarySelfsigned = false;
+    defaults = {
+      email = "ahwx@ahwx.org";
+    };
+  };
 
   services.nextcloud = {
     enable = true;
-    home = "/home/liv/nextcloud";
+    home = "/var/nextcloud/home";
     https = true;
-    configureRedis = true; # caching
+    configureRedis = false; # caching
     maxUploadSize = "10G";
     hostName = "cloud.liv.town";
+
+    config = {
+      adminuser = "liv";
+      adminpassFile = "/var/nextcloud/AdminPass";
+    };
 
     settings = {
       trusted_domains = [
         "cloud.liv.town"
       ];
+      enabledPreviewProviders = [
+        "OC\\Preview\\BMP"
+        "OC\\Preview\\GIF"
+        "OC\\Preview\\JPEG"
+        "OC\\Preview\\Krita"
+        "OC\\Preview\\MarkDown"
+        "OC\\Preview\\MP3"
+        "OC\\Preview\\OpenDocument"
+        "OC\\Preview\\PNG"
+        "OC\\Preview\\TXT"
+        "OC\\Preview\\XBitmap"
+        "OC\\Preview\\HEIC"
+      ];
     };
-
-    extraOptions.enabledPreviewProviders = [
-      "OC\\Preview\\BMP"
-      "OC\\Preview\\GIF"
-      "OC\\Preview\\JPEG"
-      "OC\\Preview\\Krita"
-      "OC\\Preview\\MarkDown"
-      "OC\\Preview\\MP3"
-      "OC\\Preview\\OpenDocument"
-      "OC\\Preview\\PNG"
-      "OC\\Preview\\TXT"
-      "OC\\Preview\\XBitmap"
-      "OC\\Preview\\HEIC"
-    ];
   };
 
-  security.acme = {
-    acceptTerms = true;   
-    certs = { 
-      ${config.services.nextcloud.hostName}.email = "ahwx@ahwx.org"; 
-    }; 
+  services.nginx = {
+    enable = true;
+    recommendedGzipSettings = true;
+    recommendedOptimisation = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+    virtualHosts.${config.services.nextcloud.hostName} = {
+      enableACME = true;
+      addSSL = true;
+      locations."/" = {
+        proxyPass = "http://localhost:8080";
+        proxyWebsockets = true;
+      };
+    };
   };
 }
