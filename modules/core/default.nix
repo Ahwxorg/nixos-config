1b79aa0860a876cb07baace473ef5af54a703cc9
Ahwx <ahwx@ahwx.org>
2024-07-22 11:17:32 +0200

    feat: adds back stuff i didnt need on server

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -1,19 +1,19 @@
 { ... }:
 {
   imports =
-       # [(import ./bootloader.nix)]
-       [(import ./hardware.nix)]
-    # ++ [(import ./xserver.nix)]
+       [(import ./bootloader.nix)]
+    ++ [(import ./hardware.nix)]
+    ++ [(import ./xserver.nix)]
     ++ [(import ./network.nix)]
-    # ++ [(import ./pipewire.nix)]
+    ++ [(import ./pipewire.nix)]
     ++ [(import ./program.nix)]
     ++ [(import ./sshd.nix)]
     ++ [(import ./security.nix)]
     ++ [(import ./services.nix)]
     ++ [(import ./system.nix)]
-    ++ [(import ./user.nix)];
-    # ++ [(import ./bluetooth.nix)]
-    # ++ [(import ./yubikey.nix)]
-    # ++ [(import ./steam.nix)]
-    # ++ [(import ./wayland.nix)];
+    ++ [(import ./user.nix)]
+    ++ [(import ./bluetooth.nix)]
+    ++ [(import ./yubikey.nix)]
+    ++ [(import ./steam.nix)]
+    ++ [(import ./wayland.nix)];
 }
