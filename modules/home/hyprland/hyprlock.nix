f2c95fd04674904be774e9e1ac894c6658be2fdb
Ahwx <ahwx@ahwx.org>
2024-08-18 16:14:15 +0200

    feat: use prettier lockscreen

--- modules/home/hyprland/hyprlock.nix
+++ modules/home/hyprland/hyprlock.nix
@@ -5,86 +5,102 @@
 
     extraConfig = ''
       source = $HOME/nixos-config/modules/home/hyprland/mocha.conf
-      
-      $accent = $mauve
-      $accentAlpha = $mauveAlpha
+
+      $accent = 0xb3$tealAlpha
+      $accentAlpha = $tealAlpha
       $font = JetBrainsMono Nerd Font
       
       # GENERAL
       general {
-        disable_loading_bar = true
-        hide_cursor = true
+          disable_loading_bar = true
+          hide_cursor = true
       }
       
       # BACKGROUND
       background {
-        monitor =
-        path = ~/nixos-config/modules/home/hyprland/lockscreen.png
-        blur_passes = 0
-        color = $base
+          monitor =
+          path = ~/Pictures/wallpapers/others/street-by-ahwx.jpg
+          blur_passes = 2
+          color = $base
       }
       
       # TIME
-      #label {
-      #    monitor =
-      #    text = cmd[update:30000] echo "$(date +"%R")"
-      #    color = $text
-      #    font_size = 90
-      #    font_family = $font
-      #    position = -30, 0
-      #    halign = right
-      #    valign = top
-      #}
+      label {
+          monitor =
+          text = cmd[update:30000] echo "$(date +"%R")"
+          color = $text
+          font_size = 90
+          font_family = $font
+          position = -130, -100
+          halign = right
+          valign = top
+          shadow_passes = 2
+      }
       
       # DATE 
-      #label {
-      #    monitor = 
-      #    text = cmd[update:43200000] echo "$(date +"%A, %d %B %Y")"
-      #    color = $text
-      #    font_size = 25
-      #    font_family = $font
-      #    position = -30, -150
-      #    halign = right
-      #    valign = top
-      #}
+      label {
+          monitor = 
+          text = cmd[update:43200000] echo "$(date +"%A, %d %B %Y")"
+          color = $text
+          font_size = 25
+          font_family = $font
+          position = -130, -250
+          halign = right
+          valign = top
+          shadow_passes = 2
+      }
+      
+      # KEYBOARD LAYOUT
+      label {
+          monitor =
+          text = $LAYOUT
+          color = $text
+          font_size = 20
+          font_family = $font
+          rotate = 0 # degrees, counter-clockwise
+      
+          position = -130, -310
+          halign = right
+          valign = top
+          shadow_passes = 2
+      }
       
       # USER AVATAR
+      image {
+          monitor = 
+          path = $HOME/.face
+          size = 350
+          border_color = $accent
+          rounding = -1
       
-      #image {
-      #    monitor = 
-      #    path = ~/.face
-      #    size = 100
-      #    border_color = $accent
-      #
-      #    position = 0, 75
-      #    halign = center
-      #    valign = center
-      #}
+          position = 0, 75
+          halign = center
+          valign = center
+          shadow_passes = 2
+      }
       
       # INPUT FIELD
       input-field {
-        monitor =
-        size = 300, 60
-        outline_thickness = 4
-        dots_size = 0.2
-        dots_spacing = 0.5
-        dots_center = true
-        outer_color = $accent
-        inner_color = rgba(24, 24, 36, 1)
-       #inner_color = $surface0
-        font_color = rgba(203, 164, 243, 1)
-       #font_color = $text
-        fade_on_empty = false
-        placeholder_text = <span foreground="##$accentAlpha">󰌾 <span foreground="##$accentAlpha"><b>$USER</b></span></span>
-       #placeholder_text = <span foreground="##$textAlpha"><i>󰌾 Logged in as </i><span foreground="##$accentAlpha">$USER</span></span>
-        hide_input = false
-        check_color = $accent
-        fail_color = $red
-        fail_text = <i>$FAIL <b>($ATTEMPTS)</b></i>
-        capslock_color = $yellow
-        position = 0, -48
-        halign = center
-        valign = center
+          monitor =
+          size = 400, 70
+          outline_thickness = 4
+          dots_size = 0.2
+          dots_spacing = 0.2
+          dots_center = true
+          outer_color = $accent
+          inner_color = $surface0
+          font_color = $text
+          fade_on_empty = false
+          placeholder_text = <span foreground="##$textAlpha">󰌾 Logged in as $USER</span>
+          hide_input = false
+          check_color = $accent
+          fail_color = $red
+          fail_text = <i>$FAIL <b>($ATTEMPTS)</b></i>
+          capslock_color = $yellow
+          position = 0, -185
+          halign = center
+          valign = center
+          shadow_passes = 2
       }'';
   };
 }
