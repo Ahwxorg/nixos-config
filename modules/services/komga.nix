990f3af23ef294942c3d173e7ab9e1804a8b40b7
Ahwx <ahwx@ahwx.org>
2024-09-26 13:18:44 +0200

    feat: adds komga for books

---
+++ modules/services/komga.nix
@@ -0,0 +1,20 @@
+{ globals, lib, config, pkgs, ... }: {
+  services.komga = {
+    enable = true;
+    port = 2872;
+    stateDir = "/var/lib/komga";
+    openFirewall = true;
+    user = globals.user;
+  };
+
+  services = {
+    nginx.virtualHosts."read.liv.town" = {
+      useACMEHost = "liv.town";
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://localhost:2872";
+      };
+    };
+  };
+
+}
