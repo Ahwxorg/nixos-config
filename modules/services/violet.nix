76bfe183fa2999d450ba86db886dfda3b8c859cd
Ahwx <ahwx@ahwx.org>
2024-10-07 21:30:13 +0200

    feat: adds some services :)

--- modules/services/violet.nix
+++ modules/services/violet.nix
@@ -2,6 +2,15 @@
   imports =
        [(import ./invidious.nix)]
     ++ [(import ./mumble.nix)]
+    ++ [(import ./ntfy.nix)]
+    ++ [(import ./sharkey-proxy.nix)]
+    ++ [(import ./monitoring.nix)]
+    ++ [(import ./docker.nix)]
+    ++ [(import ./gokapi.nix)]
+    ++ [(import ./nginx.nix)]
+    ++ [(import ./komga.nix)]
+    ++ [(import ./frp.nix)]
+    ++ [(import ./nextcloud.nix)]
     ++ [(import ./matrix/default.nix)];
     # ++ [(import ./tmux.nix)];
 }
