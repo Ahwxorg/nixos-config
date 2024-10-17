451188b5992ffaa8e55b9f44b9494dc218d3727a
Ahwx <ahwx@ahwx.org>
2024-10-17 11:13:28 +0200

    chore: also remove from defaults

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,13 +1,12 @@
 { ...}: {
   imports =
-       [(import ./bat.nix)]                       # better cat command
-    ++ [(import ./git.nix)]                       # version control
+       [(import ./git.nix)]                       # version control
     ++ [(import ./swaync/default.nix)]            # notification panel
     ++ [(import ./hyprland)]                      # window manager
     ++ [(import ./kitty.nix)]                     # terminal
     ++ [(import ./mako.nix)]                      # notification deamon
     ++ [(import ./nvim.nix)]                      # neovim editor
-    ++ [(import ./zathura.nix)]                      # neovim editor
+    ++ [(import ./zathura.nix)]                   # neovim editor
     ++ [(import ./packages.nix)]                  # other packages
     ++ [(import ./scripts/scripts.nix)]           # personal scripts
     ++ [(import ./swaylock.nix)]                  # lock screen
