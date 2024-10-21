7d4ca49605c3494946ecedfecc0138092644ab13
Ahwx <ahwx@ahwx.org>
2024-10-21 22:58:12 +0200

    feat: move lots over to laptop role

--- hosts/sakura/default.nix
+++ hosts/sakura/default.nix
@@ -1,27 +1,23 @@
-{ inputs, ... }: 
+{ inputs, pkgs, config, ... }: 
 {
   imports = [
     ./hardware-configuration.nix
     ./../../modules/core
     inputs.nixos-hardware.nixosModules.framework-13-7040-amd
   ];
+  
+  liv.laptop.enable = true;
 
   hardware.framework.amd-7040.preventWakeOnAC = true;
   networking.hostName = "sakura";
 
-  liv.laptop = true;
-
-  services = {    
-    thermald.enable = true;
-    cpupower-gui.enable = true;
-    # power-profiles-daemon.enable = true;
- 
-    upower = {
-      enable = true;
-      percentageLow = 20;
-      percentageCritical = 5;
-      percentageAction = 3;
-      criticalPowerAction = "PowerOff";
-    };
+  boot = {
+    kernelModules = ["acpi_call"];
+    extraModulePackages = with config.boot.kernelPackages;
+      [
+        acpi_call
+        cpupower
+      ]
+      ++ [pkgs.cpupower-gui];
   };
 }
