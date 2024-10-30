97e9cfc11cbb6073ede05c41827cb9d8c9d06f8f
Ahwx <ahwx@ahwx.org>
2024-10-30 12:07:54 +0100

    feat: adds server role

---
+++ roles/server.nix
@@ -0,0 +1,36 @@
+{ lib, pkgs, config, username, home-manager, ... }:
+with lib;
+let
+  cfg = config.liv.server;
+in {
+  options.liv.server = {
+    enable = mkEnableOption "Enable server";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      pkgs.kitty.terminfo
+      powertop
+    ];
+    
+    services = {    
+      thermald.enable = true;
+      # cpupower-gui.enable = true;
+      # power-profiles-daemon.enable = true;
+
+      # auto-cpufreq = {
+      #   enable = true;
+      #   settings = {
+      #     battery = {
+      #       governor = "powersave";
+      #       turbo = "auto";
+      #     };
+      #     charger = {
+      #       governor = "performance";
+      #       turbo = "auto";
+      #     };
+      #   };
+      # };
+    };
+  };
+}
