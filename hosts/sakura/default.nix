a4ef411f2c4ab09df3a95a2401dd7ce05d191b64
Ahwx <ahwx@ahwx.org>
2024-11-14 00:43:39 +0100

    feat: adds more framework-specific options to `sakura`

--- hosts/sakura/default.nix
+++ hosts/sakura/default.nix
@@ -1,4 +1,4 @@
-{ inputs, pkgs, config, ... }: 
+{ inputs, pkgs, config, lib, ... }: 
 {
   imports = [
     ./hardware-configuration.nix
@@ -6,12 +6,23 @@
     inputs.nixos-hardware.nixosModules.framework-13-7040-amd
   ];
   
+  powerManagement = {
+    enable = true;
+    # powertop.enable = true;
+    cpuFreqGovernor = lib.mkDefault "ondemand";
+  };
+
+  environment.systemPackages = with pkgs; [
+    fwupd                                           # Update firmware for Framework Laptop 13
+  ];
+
   liv.laptop.enable = true;
 
   hardware.framework.amd-7040.preventWakeOnAC = true;
   networking.hostName = "sakura";
 
   boot = {
+    kernelParams = [ "mem_sleep_default=deep" ];
     kernelModules = ["acpi_call"];
     extraModulePackages = with config.boot.kernelPackages;
       [
