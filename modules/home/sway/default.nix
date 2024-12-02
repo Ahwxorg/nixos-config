b6eebba27a964d583d1bc44631948d65c416584b
Ahwx <ahwx@ahwx.org>
2024-12-02 11:55:22 +0100

    chore: adds old stuff that i had added but is no longer required now :p

--- modules/home/sway/default.nix
+++ modules/home/sway/default.nix
@@ -1,6 +1,20 @@
-{ pkgs, ... }: {
+{ pkgs, username, ... }: {
   home.packages = with pkgs; [
     swayfx
     autotiling
+    # libinput-gestures
+    # wmctrl
   ];
+  #home.file = {
+  #  "/home/${username}/.config/libinput-gestures/sway.conf" = {
+  #    executable = false;
+  #    text = "
+  #      # Cycle right through sway workspaces
+  #      gesture: swipe right 3 swaymsg focus right
+
+  #      # Cycle left through sway workspaces
+  #      gesture: swipe left 3 swaymsg focus left
+  #    ";
+  #  };
+  #};
 }
