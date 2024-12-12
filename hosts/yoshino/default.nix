5184109dbac75168fd10a4264a7bd4185b3011fe
Ahwx <ahwx@ahwx.org>
2024-12-12 22:24:17 +0100

    feat: adds `yoshino` (desktop)

---
+++ hosts/yoshino/default.nix
@@ -0,0 +1,31 @@
+{ inputs, pkgs, config, lib, ... }: 
+{
+  imports = [
+    ./hardware-configuration.nix
+    ./../../modules/core
+  ];
+  
+  powerManagement = {
+    enable = true;
+    # powertop.enable = true;
+    cpuFreqGovernor = lib.mkDefault "performance";
+  };
+
+  liv.laptop.enable = false;
+
+  networking = {
+    hostName = "yoshino";
+    networkmanager.enable = true;
+  };
+
+  boot = {
+    kernelParams = [ ];
+    kernelModules = ["acpi_call"];
+    extraModulePackages = with config.boot.kernelPackages;
+      [
+        acpi_call
+        cpupower
+      ]
+      ++ [pkgs.cpupower-gui];
+  };
+}
