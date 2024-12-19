1f65af50d65b53d146fc6ff4f4836e5ec746734f
Ahwx <ahwx@ahwx.org>
2024-12-19 18:18:14 +0100

    fix: make radicale work woah

--- modules/services/radicale.nix
+++ modules/services/radicale.nix
@@ -6,7 +6,7 @@
       server.hosts = [ "0.0.0.0:5232" ];
       auth = {
         type = "htpasswd";
-        htpasswd_filename = "/home/liv/radicaleusers";
+        htpasswd_filename = "/etc/radicale/htpasswd";
         htpasswd_encryption = "bcrypt";
       };
     };
@@ -17,18 +17,20 @@
     recommendedProxySettings = true;
     recommendedTlsSettings = true;
     virtualHosts = {
-      "calendar.liv.town" = {
+      "plan.liv.town" = {
         forceSSL = true;
         sslCertificate = "/var/lib/acme/liv.town/cert.pem";
         sslCertificateKey = "/var/lib/acme/liv.town/key.pem";
         # locations."/radicale/" = {
         locations."/" = {
           proxyPass = "http://127.0.0.1:5232/";
-          # extraConfig = ''
-          #   # proxy_set_header X-Script-Name /radicale;
-          #   # proxy_set_header X-Script-Name /;
-          #   proxy_pass_header Authorization;
-          # '';
+          proxyWebsockets = true;
+          extraConfig = ''
+            proxy_connect_timeout       300;
+            proxy_send_timeout          300;
+            proxy_read_timeout          300;
+            send_timeout                300;
+          '';
         };
       };
     };
