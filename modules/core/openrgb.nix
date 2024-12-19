2faaa98f66379fad48280a03e8f09bc5d3c71691
Ahwx <ahwx@ahwx.org>
2024-12-20 00:23:23 +0100

    feat: adds openrgb service

---
+++ modules/core/openrgb.nix
@@ -0,0 +1,12 @@
+{ pkgs, ... }:
+{
+  services.hardware.openrgb = { 
+    enable = true; 
+    package = pkgs.openrgb-with-all-plugins; 
+    motherboard = "amd"; 
+    server = { 
+      port = 6742; 
+      # autoStart = true; 
+    }; 
+  };
+}
