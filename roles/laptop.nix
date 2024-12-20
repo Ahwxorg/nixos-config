19215b301b1c0e34d96ffc6e3f95fd256688570e
Ahwx <ahwx@ahwx.org>
2024-12-20 20:12:00 +0100

    fix: enable `thermald.service` for laptops, remove old code

--- roles/laptop.nix
+++ roles/laptop.nix
@@ -11,7 +11,6 @@ in {
     home-manager = {
       users.${username} = {
         home.packages = with pkgs; [
-          # reader
           acpi
           brightnessctl
         ];
@@ -31,7 +30,7 @@ in {
         ];
       };
     services = {    
-      # thermald.enable = true; # Enable if on Intel, should be a if-statement.
+      thermald.enable = true;
       power-profiles-daemon.enable = true;
  
       upower = {
@@ -45,76 +44,3 @@ in {
     # powerManagement.powertop.enable = true;
   };
 }
-
-
-
-# { config, pkgs, lib, ... }:
-# with lib;
-# let
-#   cfg = config.liv.profiles;
-#   laptopPkgs = with pkgs; [
-#     acpi
-#     brightnessctl
-#   ];
-# in
-# {
-#   options = {
-#     liv-laptop = lib.mkOption {
-#       default = false;
-#       type = lib.types.bool;
-#       description = ''
-#         Enable this if the host is a laptop, to enable power management, extra packages, kernel modules, etc.
-#       '';
-#     };
-#   };
-# 
-#   config = lib.mkIf cfg.liv-laptop {
-#     home.packages = with pkgs; [
-#       # reader
-#       vlc
-#     ] ++ optionals cfg.liv-laptop laptopPkgs;
-# 
-# 
-#     networking.networkmanager.enable = true;
-# 
-#     environment.systemPackages = with pkgs; [
-#       cpupower-gui
-#       powertop
-#     ];
-#     # auto-cpufreq = {
-#     #   enable = false;
-#     #   settings = {
-#     #     battery = {
-#     #       governor = "powersave";
-#     #       turbo = "auto";
-#     #     };
-#     #     charger = {
-#     #       governor = "performance";
-#     #       turbo = "auto";
-#     #     };
-#     #   };
-#     # };
-#     boot = {
-#       kernelModules = ["acpi_call"];
-#       extraModulePackages = with config.boot.kernelPackages;
-#         [
-#           acpi_call
-#           cpupower
-#         ]
-#         ++ [pkgs.cpupower-gui];
-#       };
-#     services = {    
-#       thermald.enable = true;
-#       cpupower-gui.enable = true;
-#       # power-profiles-daemon.enable = true;
-#  
-#       upower = {
-#         enable = true;
-#         percentageLow = 20;
-#         percentageCritical = 5;
-#         percentageAction = 3;
-#         criticalPowerAction = "PowerOff";
-#       };
-#     };
-#   };
-# }
