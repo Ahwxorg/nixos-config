8e000d6729d2399463f9ef235e93c448a499ff4a
Ahwx <ahwx@ahwx.org>
2024-12-20 20:12:16 +0100

    feat: make desktop role

--- roles/desktop.nix
+++ roles/desktop.nix
@@ -1,4 +1,32 @@
-{ ... }: {
-  # system.powerManagement.cpuFreqGovernor = "performance";
-}
+{ lib, pkgs, config, username, home-manager, ... }:
+with lib;
+let
+  cfg = config.liv.desktop;
+in {
+  options.liv.desktop = {
+    enable = mkEnableOption "Enable desktop";
+  };
 
+  config = mkIf cfg.enable {
+    home-manager = {
+      users.${username} = {
+        home.packages = with pkgs; [
+          # Home packages
+        ];
+      };
+    };
+
+    networking.networkmanager.enable = true;
+
+    boot = {
+      kernelModules = ["acpi_call"];
+      extraModulePackages = with config.boot.kernelPackages;
+        [
+          acpi_call
+        ];
+      };
+    services = {    
+      thermald.enable = true;
+    };
+  };
+}
