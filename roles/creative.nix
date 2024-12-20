ac991e3860e8b5a315cbe3e3386e361f69572a79
Ahwx <ahwx@ahwx.org>
2024-12-20 20:11:22 +0100

    feat: adds creative role

---
+++ roles/creative.nix
@@ -0,0 +1,23 @@
+{ lib, pkgs, config, username, home-manager, ... }:
+with lib;
+let
+  cfg = config.liv.creative;
+in {
+  options.liv.creative = {
+    enable = mkEnableOption "Enable creative workflow";
+  };
+
+  config = mkIf cfg.enable {
+    home-manager = {
+      users.${username} = {
+        home.packages = with pkgs; [
+          gimp
+          darktable
+          audacity
+          obs-studio
+          kdenlive
+        ];
+      };
+    };
+  };
+}
