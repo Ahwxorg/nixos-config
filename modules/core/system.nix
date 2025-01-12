b290a449da887bfae46b5b36bb5e8f7a6df88789
Ahwx <ahwx@ahwx.org>
2025-01-12 17:22:27 +0100

    feat: adds japanese as a locale so that things display correctly hopefully

--- modules/core/system.nix
+++ modules/core/system.nix
@@ -38,5 +38,9 @@
 
   time.timeZone = "Europe/Amsterdam";
   i18n.defaultLocale = "en_US.UTF-8";
+  i18n.supportedLocales = [
+    "en_US.UTF-8/UTF-8"
+    "ja_JP.UTF-8/UTF-8"
+  ];
   system.stateVersion = "24.05";
 }
