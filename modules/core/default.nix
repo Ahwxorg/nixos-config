d536a902dd8702c78f75b2da072958ce92b58dc7
Ahwx <ahwx@ahwx.org>
2025-01-12 17:22:37 +0100

    feat: enable displaylink for all devices

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -4,6 +4,7 @@
        [(import ./bootloader.nix)]
     ++ [(import ./docker.nix)]
     ++ [(import ./hardware.nix)]
+    ++ [(import ./displaylink.nix)]
     # ++ [(import ./openrgb.nix)]
     ++ [(import ./xserver.nix)]
     ++ [(import ./network.nix)]
