51db06e6494da9e92a5c9a1a1c08f03f06920cf4
Ahwx <ahwx@ahwx.org>
2025-02-08 11:43:04 +0100

    feat: adds jitsi-meet (though that breaks due to ssl right now)

--- modules/services/jitsi-meet.nix
+++ modules/services/jitsi-meet.nix
@@ -1,17 +1,27 @@
 { ... }:
 {
-  services.jitsi-meet = {
-    enable = true;
-    hostName = "meet.liv.town";
-    config = {
-      prejoinPageEnabled = true;
-      disableModeratorIndicator = true;
+  services = {
+    jitsi-meet = {
+      enable = true;
+      hostName = "meet.liv.town";
+      prosody.lockdown = true;
+      config = {
+        enableWelcomePage = false;
+        prejoinPageEnabled = true;
+      };
+      interfaceConfig = {
+        SHOW_JITSI_WATERMARK = false;
+        SHOW_WATERMARK_FOR_GUESTS = false;
+      };
     };
-    interfaceConfig = {
-      SHOW_JITSI_WATERMARK = false;
+    nginx.virtualHosts."meet.liv.town" = {
+      forceSSL = true;
+      sslCertificate = "/var/lib/acme/liv.town/cert.pem";
+      sslCertificateKey = "/var/lib/acme/liv.town/key.pem";
+      locations."/" = {
+        proxyPass = "http://localhost:8080";
+        proxyWebsockets = true;
+      };
     };
-    jibri.enable = false;
   };
-
-  services.jitsi-videobridge.openFirewall = true;
 }
