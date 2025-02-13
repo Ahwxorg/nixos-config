07b52feb4c2f984de4939d97e0fbc502f94bcc8f
Ahwx <ahwx@ahwx.org>
2025-02-13 11:19:07 +0100

    feat: move openrgb to desktop role

--- roles/desktop.nix
+++ roles/desktop.nix
@@ -29,4 +29,13 @@ in {
       thermald.enable = true;
     };
   };
+  services.hardware.openrgb = { 
+    enable = true; 
+    package = pkgs.openrgb-with-all-plugins; 
+    motherboard = "amd"; 
+    server = { 
+      port = 6742; 
+      # autoStart = true; 
+    }; 
+  };
 }
