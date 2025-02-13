9e60b87a788a2036cae50e043df08157556f0538
Ahwx <ahwx@ahwx.org>
2025-02-13 12:14:14 +0100

    fix: move openrgb block into config blocks so that nix likes me back

--- roles/desktop.nix
+++ roles/desktop.nix
@@ -27,15 +27,15 @@ in {
       };
     services = {    
       thermald.enable = true;
+      hardware.openrgb = { 
+        enable = true; 
+        package = pkgs.openrgb-with-all-plugins; 
+        motherboard = "amd"; 
+        server = { 
+          port = 6742; 
+          # autoStart = true; 
+        }; 
+      };
     };
-  };
-  services.hardware.openrgb = { 
-    enable = true; 
-    package = pkgs.openrgb-with-all-plugins; 
-    motherboard = "amd"; 
-    server = { 
-      port = 6742; 
-      # autoStart = true; 
-    }; 
   };
 }
