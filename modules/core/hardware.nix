2a771587a69284a65a08caab0d665f6aefff005f
Ahwx <ahwx@ahwx.org>
2025-02-26 23:42:37 +0100

    chore: remove some legacy commented out code

--- modules/core/hardware.nix
+++ modules/core/hardware.nix
@@ -3,8 +3,6 @@
   hardware = {
     graphics = {
       enable = true;
-      # driSupport = true; # Has no effect anymore, as of 2024-06-21
-      # driSupport32Bit = true;
     };
     enableRedistributableFirmware = true;
     opengl = {
