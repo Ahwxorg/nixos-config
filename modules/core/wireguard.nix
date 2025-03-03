3fbc341b84970fe8321ce1322a4003152a7b9b3a
Ahwx <ahwx@ahwx.org>
2025-03-03 13:19:18 +0100

    chore: move to other wireguard instance

--- modules/core/wireguard.nix
+++ modules/core/wireguard.nix
@@ -2,15 +2,15 @@
 {
   networking.wg-quick.interfaces = {
     wg0 = {
-      address = [ "10.7.0.2/24" "fddd:2c4:2c4:2c4::2/64" ];
+      address = [ "10.7.0.4/24" ];
       dns = [ "9.9.9.9" "149.112.112.112" ];
       privateKeyFile = "/root/wireguard-keys/privatekey";
       
       peers = [{
-        publicKey = "GfrFhe2JV8FS/711WAdx6CLF/QIEj1KoOGP/ErxBHkg=";
+        publicKey = "uE40chWhiPpnNHcgnLhMfOUfzotS6hK+dWwI1sIFcUw=";
         presharedKeyFile = "/root/wireguard-keys/preshared_from_peer0_key";
         allowedIPs = [ "0.0.0.0/0" "::/0" ];
-        endpoint = "57.129.46.171:51820";
+        endpoint = "213.210.34.27:58192";
         persistentKeepalive = 25;
       }];
     };
