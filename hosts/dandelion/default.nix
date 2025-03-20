fe6e5d4c57daa4f56efaf518c3686918c69c59e7
Ahwx <ahwx@ahwx.org>
2025-03-20 14:27:04 +0100

    feat: adds `dandelion` host

---
+++ hosts/dandelion/default.nix
@@ -0,0 +1,27 @@
+{ pkgs, config, ... }: 
+{
+  imports = [
+    ./hardware-configuration.nix
+    ./../../modules/core/default.dandelion.nix
+  ];
+
+  networking.hostName = "dandelion";
+
+  nixpkgs.config.permittedInsecurePackages = [
+    "jitsi-meet-1.0.8043"
+    "olm-3.2.16"
+  ];
+
+  time.timeZone = "Europe/Amsterdam";
+
+  environment.systemPackages = with pkgs; [
+    pkgs.kitty.terminfo
+  ];
+
+  boot = {
+    loader.systemd-boot = {
+      enable = true;
+      canTouchEfiVariables = true;
+    };
+  };
+}
