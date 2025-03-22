4fcf563b4b85c81b2a593d57d6ea7987126ba62c
Ahwx <ahwx@ahwx.org>
2025-03-22 22:45:36 +0100

    feat: adds system-wide variables

---
+++ variables.nix
@@ -0,0 +1,17 @@
+{
+  lib,
+  config,
+  ...
+}: let
+  inherit (lib) mkOption types;
+  inherit (config.liv) variables;
+in {
+  options.liv.variables = {
+    primaryDomain = mkOption {
+      default = "liv.town";
+      type = types.str;
+      readOnly = true;
+      description = "My primary domain";
+    };
+  };
+}
