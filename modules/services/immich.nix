df1ba52d7db0e843b7c5b8721c65282000c5796f
Ahwx <ahwx@ahwx.org>
2025-03-23 00:05:36 +0100

    feat: adds immich configuration to services

---
+++ modules/services/immich.nix
@@ -0,0 +1,23 @@
+{ config, ... }: {
+  services.immich = {
+    enable = true;
+    port = 2283;
+    accelerationDevices = /dev/dri/card0;
+  };
+
+  # services.nginx.virtualHosts."" = {
+  #   enableACME = true;
+  #   forceSSL = true;
+  #   locations."/" = {
+  #     proxyPass = "http://localhost:${toString config.services.immich.port}";
+  #     proxyWebsockets = true;
+  #     recommendedProxySettings = true;
+  #     extraConfig = ''
+  #       client_max_body_size 50000M;
+  #       proxy_read_timeout   600s;
+  #       proxy_send_timeout   600s;
+  #       send_timeout         600s;
+  #     '';
+  #   };
+  # };
+}
