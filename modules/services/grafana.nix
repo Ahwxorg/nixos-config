102e83b16962e17c06d911ed4a203ce4e44711bc
Ahwx <ahwx@ahwx.org>
2025-04-24 11:40:52 +0200

    feat: move grafana to its own config so i can disable it easier

---
+++ modules/services/grafana.nix
@@ -0,0 +1,22 @@
+{ config, ... }:
+{
+  services = {
+    grafana = {
+      enable = true;
+      settings.server = {
+        domain = "monitoring.liv.town";
+        http_addr = "127.0.0.1";
+        http_port = 2342;
+      };
+    };
+
+    nginx.virtualHosts.${config.services.grafana.domain} = {
+      useACMEHost = "liv.town";
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:${toString config.services.grafana.port}";
+        proxyWebsockets = true;
+      };
+    };
+  }:
+}
--- modules/services/monitoring.nix
+++ modules/services/monitoring.nix
@@ -1,23 +1,6 @@
-{ config, ... }: {
+{ config, ... }:
+{
   services = {
-    grafana = {
-      enable = true;
-      settings.server = {
-        domain = "monitoring.liv.town";
-        http_addr = "127.0.0.1";
-        http_port = 2342;
-      };
-    };
-
-    nginx.virtualHosts.${config.services.grafana.domain} = {
-      useACMEHost = "liv.town";
-      forceSSL = true;
-      locations."/" = {
-        proxyPass = "http://127.0.0.1:${toString config.services.grafana.port}";
-        proxyWebsockets = true;
-      };
-    };
-
     prometheus = {
       enable = true;
       port = 9001;
@@ -28,13 +11,16 @@
           port = 9002;
         };
       };
-      scrapeConfigs = [{
-        job_name = "violet";
-        static_configs = [{
-          targets = [ "127.0.0.1:${toString config.services.prometheus.exporters.node.port}" ];
-        }];
-      }];
+      scrapeConfigs = [
+        {
+          job_name = "${config.networking.hostname}";
+          static_configs = [
+            {
+              targets = [ "127.0.0.1:${toString config.services.prometheus.exporters.node.port}" ];
+            }
+          ];
+        }
+      ];
     };
   };
 }
-
