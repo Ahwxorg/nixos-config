22d4694bbd3b7e914c75859bca6ebaeda383e03b
Ahwx <ahwx@ahwx.org>
2025-04-24 16:40:11 +0200

    fix: move `hardware.nvidia.open` down a level so it works

--- roles/nvidia.nix
+++ roles/nvidia.nix
@@ -16,15 +16,15 @@ in
   config = mkIf cfg.enable {
     hardware = {
       enableRedistributableFirmware = true;
+      nvidia.open = false; # Set to false/true for proprietary/open drivers
       graphics = {
         enable = true;
-        nvidia.open = false; # Set to false/true for proprietary/open drivers
         extraPackages = with pkgs; [
-          # amdvlk
+          # amdvlk would be the package that would be required for AMD on desktop, since none of the hosts use NVIDIA as a desktop GPU, this is open and uncertain.
         ];
         # For 32 bit applications as well
         extraPackages32 = with pkgs; [
-          # driversi686Linux.amdvlk
+          # driversi686Linux.amdvlk would be the package that would be required for AMD on desktop, since none of the hosts use NVIDIA as a desktop GPU, this is open and uncertain.
         ];
       };
     };
