1813d438a693dea9a294e3ddecc84cc888233811
Ahwx <ahwx@ahwx.org>
2025-04-25 12:05:53 +0200

    chore: remove `hplip` since it didn't work anyways

--- modules/core/printing.nix
+++ modules/core/printing.nix
@@ -1,11 +1,10 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   services.avahi = {
     enable = true;
     nssmdns4 = true;
     openFirewall = true;
   };
-  environment.systemPackages = with pkgs; [
-    hplip
-    hplipWithPlugin
-  ];
+  # environment.systemPackages = with pkgs; [
+  # ];
 }
