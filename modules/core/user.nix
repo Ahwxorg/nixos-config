d6ee269fe9a9f9f7f328b28c8131c0c2fd084c44
Ahwx <ahwx@ahwx.org>
2025-04-25 12:13:44 +0200

    refactor: comply with new formatting :)

--- modules/core/user.nix
+++ modules/core/user.nix
@@ -1,26 +1,37 @@
-{ pkgs, inputs, config, username, host, ...}:
 {
-  imports = [ inputs.home-manager.nixosModules.home-manager ] ++ [ ./../../roles ] ++ [./../../variables.nix];
+  pkgs,
+  inputs,
+  config,
+  username,
+  host,
+  ...
+}:
+{
+  imports =
+    [ inputs.home-manager.nixosModules.home-manager ]
+    ++ [ ./../../roles/default.nix ]
+    ++ [ ./../../variables.nix ];
   home-manager = {
     useUserPackages = true;
     useGlobalPkgs = true;
     extraSpecialArgs = { inherit inputs username host; };
     users.${username} = {
-      imports = 
-        if (host == "desktop") then 
-          [ ./../home/default.desktop.nix ] 
+      imports =
+        if (host == "desktop") then
+          [ ./../home/default.desktop.nix ]
         else if (host == "violet") then
           [ ./../home/default.server.nix ]
         else if (host == "dandelion") then
           [ ./../home/default.server.nix ]
         # else if (host == "yoshino") then
-          # [ ./../home/default.nix ]
-        else [ ./../home ];
-        home = {
-          username = "${username}";
-          homeDirectory = "/home/${username}";
-          stateVersion = "22.11";
-        };
+        # [ ./../home/default.nix ]
+        else
+          [ ./../home ];
+      home = {
+        username = "${username}";
+        homeDirectory = "/home/${username}";
+        stateVersion = "22.11";
+      };
       programs.home-manager.enable = true;
     };
   };
@@ -28,7 +39,12 @@
   users.users.${username} = {
     isNormalUser = true;
     description = "${username}";
-    extraGroups = [ "networkmanager" "wheel" "docker" "input" ];
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "docker"
+      "input"
+    ];
     shell = pkgs.zsh;
   };
   nix.settings.allowed-users = [ "${username}" ];
