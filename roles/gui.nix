9eef4733cb594b252652575696610a2f12a26111
Ahwx <ahwx@ahwx.org>
2025-04-28 10:19:10 +0200

    fix: actually add enable option to `options.liv.gui`...

--- roles/gui.nix
+++ roles/gui.nix
@@ -1,9 +1,20 @@
-{ lib, ... }:
+{
+  lib,
+  pkgs,
+  config,
+  username,
+  home-manager,
+  ...
+}:
 with lib;
 let
   cfg = config.liv.gui;
 in
 {
+  options.liv.gui = {
+    enable = mkEnableOption "Enable GUI workflow";
+  };
+
   config = mkIf cfg.enable {
     services = {
       gvfs.enable = true;
