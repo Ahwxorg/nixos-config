b9e2e56206eeca31026be0d7465014a5ed31bda9
Ahwx <ahwx@ahwx.org>
2025-05-09 03:44:04 +0200

    chore: displaylink is not required at the moment and takes lots of cpu

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -1,21 +1,21 @@
 { ... }:
 {
   imports =
-       [(import ./docker.nix)]
-    ++ [(import ./hardware.nix)]
-    ++ [(import ./displaylink.nix)]
+    [ (import ./docker.nix) ]
+    ++ [ (import ./hardware.nix) ]
+    # ++ [(import ./displaylink.nix)]
     # ++ [(import ./printing.nix)]
-    ++ [(import ./xserver.nix)]
-    ++ [(import ./network.nix)]
-    ++ [(import ./pipewire.nix)]
-    ++ [(import ./program.nix)]
-    ++ [(import ./sshd.nix)]
-    ++ [(import ./security.nix)]
-    ++ [(import ./services.nix)]
-    ++ [(import ./system.nix)]
-    ++ [(import ./user.nix)]
-    ++ [(import ./bluetooth.nix)]
-    ++ [(import ./yubikey.nix)]
-    ++ [(import ./steam.nix)]
-    ++ [(import ./wayland.nix)];
+    ++ [ (import ./xserver.nix) ]
+    ++ [ (import ./network.nix) ]
+    ++ [ (import ./pipewire.nix) ]
+    ++ [ (import ./program.nix) ]
+    ++ [ (import ./sshd.nix) ]
+    ++ [ (import ./security.nix) ]
+    ++ [ (import ./services.nix) ]
+    ++ [ (import ./system.nix) ]
+    ++ [ (import ./user.nix) ]
+    ++ [ (import ./bluetooth.nix) ]
+    ++ [ (import ./yubikey.nix) ]
+    ++ [ (import ./steam.nix) ]
+    ++ [ (import ./wayland.nix) ];
 }
