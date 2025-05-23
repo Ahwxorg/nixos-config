db403cde002507ea220c5e2d0c36d2d7abdbe8e7
Ahwx <ahwx@ahwx.org>
2025-05-23 03:11:14 +0200

    chore: move away from enableACME, remove unused code

--- modules/services/sharkey-proxy.nix
+++ modules/services/sharkey-proxy.nix
@@ -2,8 +2,9 @@
 {
   services = {
     nginx.virtualHosts."quack.social" = {
-      enableACME = true;
       forceSSL = true;
+      sslCertificate = "/var/lib/acme/quack.social/cert.pem";
+      sslCertificateKey = "/var/lib/acme/quack.social/key.pem";
       locations."/" = {
         proxyPass = "http://localhost:3000";
         proxyWebsockets = true;
@@ -13,26 +14,6 @@
           proxy_set_header X-Forwarded-Proto https;
           proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
           proxy_set_header X-Forwarded-Host $remote_addr;
-        '';
-      };
-
-      locations."/files/" = {
-        proxyPass = "http://localhost:3000";
-        proxyWebsockets = true;
-        extraConfig = ''
-          proxy_set_header Host $host;
-          proxy_set_header X-Real-IP $remote_addr;
-          proxy_set_header X-Forwarded-Proto https;
-          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-          proxy_set_header X-Forwarded-Host $remote_addr;
-          
-          # Try cache?
-          # proxy_cache sharkey;
-          # proxy_cache_path /var/cache/nginx/sharkey levels=1:2 keys_zone=sharkey:15m;
-          # proxy_cache_lock on;
-          # proxy_cache_use_stale updating;
-          # proxy_force_ranges on;
-          # add_header X-Cache $upstream_cache_status;
         '';
       };
 
