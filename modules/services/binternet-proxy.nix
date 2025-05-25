23e4113ed9d0e612b3dc8326b0c9b154f5522b34
Ahwx <ahwx@ahwx.org>
2025-05-25 03:57:41 +0200

    feat: adds anubis to $SERVICE

--- modules/services/binternet-proxy.nix
+++ modules/services/binternet-proxy.nix
@@ -1,12 +1,19 @@
-{ ... }:
+{ config, ... }:
 {
   services = {
+    anubis.instances.binternet = {
+      settings = {
+        TARGET = "http://localhost:8081";
+        BIND = ":8082";
+        BIND_NETWORK = "tcp";
+      };
+    };
     nginx.virtualHosts."curate.liv.town" = {
       forceSSL = true;
       sslCertificate = "/var/lib/acme/liv.town/cert.pem";
       sslCertificateKey = "/var/lib/acme/liv.town/key.pem";
       locations."/" = {
-        proxyPass = "http://localhost:8081";
+        proxyPass = "http://localhost${toString config.services.anubis.instances.binternet.settings.BIND}";
         proxyWebsockets = true;
       };
     };
--- modules/services/librey-proxy.nix
+++ modules/services/librey-proxy.nix
@@ -1,12 +1,19 @@
-{ ... }:
+{ config, ... }:
 {
   services = {
+    anubis.instances.librey = {
+      settings = {
+        TARGET = "http://localhost:8080";
+        BIND = ":8079";
+        BIND_NETWORK = "tcp";
+      };
+    };
     nginx.virtualHosts."search.liv.town" = {
       forceSSL = true;
       sslCertificate = "/var/lib/acme/liv.town/cert.pem";
       sslCertificateKey = "/var/lib/acme/liv.town/key.pem";
       locations."/" = {
-        proxyPass = "http://localhost:8080";
+        proxyPass = "http://localhost${toString config.services.anubis.instances.librey.settings.BIND}";
         proxyWebsockets = true;
       };
     };
