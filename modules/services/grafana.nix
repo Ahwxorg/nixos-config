9fee121a565190bdc41025acd08b6af01c66be86
Ahwx <ahwx@ahwx.org>
2025-05-25 03:34:51 +0200

    fix: comply with new nixos namings

--- modules/services/grafana.nix
+++ modules/services/grafana.nix
@@ -10,12 +10,12 @@
       };
     };
 
-    nginx.virtualHosts.${config.services.grafana.domain} = {
+    nginx.virtualHosts.${config.services.grafana.settings.server.domain} = {
       forceSSL = true;
       sslCertificate = "/var/lib/acme/liv.town/cert.pem";
       sslCertificateKey = "/var/lib/acme/liv.town/key.pem";
       locations."/" = {
-        proxyPass = "http://127.0.0.1:${toString config.services.grafana.port}";
+        proxyPass = "http://127.0.0.1:${toString config.services.grafana.settings.server.http_port}";
         proxyWebsockets = true;
       };
     };
