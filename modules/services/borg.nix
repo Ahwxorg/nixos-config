7a31516be1db5524329cacee83fa55bbb66f1213
Ahwx <ahwx@ahwx.org>
2025-07-01 16:47:08 +0200

    chore(temp): set address for dandelion

--- modules/services/borg.nix
+++ modules/services/borg.nix
@@ -1,7 +1,7 @@
 { pkgs, config, ... }:
 let
   hostname = "violet";
-  repo = "ssh://dandelion.booping.local:${toString config.services.openssh.ports}/spinners/rootvol/backups/${hostname}";
+  repo = "ssh://dandelion:${toString config.services.openssh.ports}/spinners/rootvol/backups/${hostname}";
 in
 {
   services.borgbackup.jobs = {
@@ -10,6 +10,7 @@ in
         "/home/liv/MinecraftDocker"
       ];
       repo = "${repo}/MinecraftDocker-tulip";
+      encryption.mode = "none";
       compression = "auto,zstd";
       startAt = "daily";
       postHook = ''
@@ -25,6 +26,7 @@ in
         "/var/lib"
       ];
       repo = "${repo}/var-lib";
+      encryption.mode = "none";
       compression = "auto,zstd";
       startAt = "daily";
       postHook = ''
@@ -35,29 +37,5 @@ in
         fi
       '';
     };
-    # "violet-random" = {
-    #   paths = [
-    #     "/random"
-    #   ];
-    #   exclude = [
-    #     "/random/a"
-    #     "/random/a"
-    #   ];
-    #  encryption = {
-    #    mode = "";
-    #    passCommand = "";
-    #  };
-    #  environment.BORG_RSH = "ssh -i ${config.sops.secrets."ssh_private_key_violet".path}";
-    #  repo = "${repo}/violet/random";
-    #  compression = "auto,zstd";
-    #  startAt = "daily";
-    #  postHook = ''
-    #    if [ $exitStatus -eq 2 ]; then
-    #      ${pkgs.ntfy-sh}/bin/ntfy send https://ntfy.${domain}/nixbox "BorgBackup: nixbox backup failed with errors"
-    #    else
-    #      ${pkgs.ntfy-sh}/bin/ntfy send https://ntfy.${domain}/nixbox "BorgBackup: nixbox backup completed succesfully with exit status $exitStatus"
-    #    fi
-    #  '';
-    # };
   };
 }
