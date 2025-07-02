713d5b2ec9c960f9f6bba90dea28de740f87b77d
Ahwx <ahwx@ahwx.org>
2025-07-02 16:02:38 +0200

    feat: upload latest version of this file to ask for help since everything is confusing

--- modules/services/borg.nix
+++ modules/services/borg.nix
@@ -14,18 +14,21 @@ in
       paths = [
         "/home/liv/MinecraftDocker"
       ];
-      repo = "${baseRepo}/MinecraftDocker-tulip";
+      repo = "ssh://liv@dandelion:9123/spinners/rootvol/backups/violet/MinecraftDocker-tulip";
       encryption.mode = "none";
       compression = "auto,zstd,10";
       startAt = [ "3:00" ];
       postHook = ''
         if [ $exitStatus -eq 2 ]; then
-          ${pkgs.ntfy-sh}/bin/ntfy send https://ntfy.liv.town/${hostname} "borgbackup: ${hostname} backup (violet-minecraft) failed with errors"
+          ${pkgs.ntfy-sh}/bin/ntfy send https://notify.liv.town/${hostname} "borgbackup: ${hostname} backup (violet-minecraft) failed with errors"
         else
-          ${pkgs.ntfy-sh}/bin/ntfy send https://ntfy.liv.town/${hostname} "borgbackup: ${hostname} backup (violet-minecraft) completed succesfully with exit status $exitStatus"
+          ${pkgs.ntfy-sh}/bin/ntfy send https://notify.liv.town/${hostname} "borgbackup: ${hostname} backup (violet-minecraft) completed succesfully with exit status $exitStatus"
         fi
       '';
       user = "${username}";
+      environment = {
+        BORG_RSH = "ssh -p 9123 -i /home/liv/.ssh/id_ed25519";
+      };
     };
     "violet-lib" = {
       paths = [
@@ -37,9 +40,9 @@ in
       startAt = "daily";
       postHook = ''
         if [ $exitStatus -eq 2 ]; then
-          ${pkgs.ntfy-sh}/bin/ntfy send https://ntfy.liv.town/${hostname} "borgbackup: ${hostname} backup (violet-lib) failed with errors"
+          ${pkgs.ntfy-sh}/bin/ntfy send https://notify.liv.town/${hostname} "borgbackup: ${hostname} backup (violet-lib) failed with errors"
         else
-          ${pkgs.ntfy-sh}/bin/ntfy send https://ntfy.liv.town/${hostname} "borgbackup: ${hostname} backup (violet-lib) completed succesfully with exit status $exitStatus"
+          ${pkgs.ntfy-sh}/bin/ntfy send https://notify.liv.town/${hostname} "borgbackup: ${hostname} backup (violet-lib) completed succesfully with exit status $exitStatus"
         fi
       '';
       user = "${username}";
