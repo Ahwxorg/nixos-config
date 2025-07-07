35db81ac4df7e934d57781345068b9d6e070712b
Ahwx <ahwx@ahwx.org>
2025-07-07 23:20:43 +0200

    fix: mpd still has issues but less than before

--- modules/services/mpd.nix
+++ modules/services/mpd.nix
@@ -7,7 +7,7 @@
 {
   services.mpd = {
     enable = true;
-    musicDirectory = "/dandelion/home/liv/music";
+    musicDirectory = "/home/liv/Music";
     extraConfig = ''
       audio_output {
         type "pipewire"
@@ -18,14 +18,14 @@
 
     # Optional:
     # network.listenAddress = "any"; # if you want to allow non-localhost connections
-    network.startWhenNeeded = false; # systemd feature: only start MPD service upon connection to its socket
   };
   systemd.services.mpd.environment = {
     # see: https://gitlab.freedesktop.org/pipewire/pipewire/-/issues/609
-    XDG_RUNTIME_DIR = "/run/user/${toString config.users.users.userRunningPipeWire.uid}"; # User-id must match above user. MPD will look inside this directory for the PipeWire socket.
+    XDG_RUNTIME_DIR = "/run/user/${toString config.users.users.${username}.uid}"; # User-id must match above user. MPD will look inside this directory for the PipeWire socket.
   };
 
-  home.packages = with pkgs; [
+  environment.systemPackages = with pkgs; [
     mpdris2
+    ncmpcpp
   ];
 }
