8ff40790f223cfc35f177d99dc8c2929c8253dac
Ahwx <ahwx@ahwx.org>
2025-07-15 11:32:53 +0200

    chore: move mpd, also add some things and reorder

--- modules/home/mpd.nix
+++ modules/home/mpd.nix
@@ -5,10 +5,19 @@
   ...
 }:
 {
+  home.packages = with pkgs; [
+    mpc
+    mpdris2
+    ncmpcpp
+  ];
+
   services.mpd = {
     enable = true;
-    musicDirectory = "/home/liv/Music";
+    musicDirectory = "/home/${username}/Music";
+    playlistDirectory = "/home/${username}/Music/.playlists";
     extraConfig = ''
+      restore_paused "yes"
+      auto_update "yes"
       audio_output {
         type "pipewire"
       	name "pipewire"
@@ -23,9 +32,4 @@
     # see: https://gitlab.freedesktop.org/pipewire/pipewire/-/issues/609
     XDG_RUNTIME_DIR = "/run/user/${toString config.users.users.${username}.uid}"; # User-id must match above user. MPD will look inside this directory for the PipeWire socket.
   };
-
-  environment.systemPackages = with pkgs; [
-    mpdris2
-    ncmpcpp
-  ];
 }
