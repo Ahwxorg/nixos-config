1b4d7303b5c60e00833590c0e9a0a4eeb3f66b71
Ahwx <ahwx@ahwx.org>
2025-07-17 11:49:01 +0200

    chore: move mpd back because it doesn't work otherwise

--- modules/services/mpd.nix
+++ modules/services/mpd.nix
@@ -5,7 +5,7 @@
   ...
 }:
 {
-  home.packages = with pkgs; [
+  environment.systemPackages = with pkgs; [
     mpc
     mpdris2
     ncmpcpp
@@ -18,9 +18,10 @@
     extraConfig = ''
       restore_paused "yes"
       auto_update "yes"
+
       audio_output {
         type "pipewire"
-      	name "pipewire"
+        name "pipewire"
       }
     '';
     user = "${username}"; # PipeWire requires this as it runs as the normal user and mpd normally runs as a system user.
@@ -30,6 +31,6 @@
   };
   systemd.services.mpd.environment = {
     # see: https://gitlab.freedesktop.org/pipewire/pipewire/-/issues/609
-    XDG_RUNTIME_DIR = "/run/user/${toString config.users.users.${username}.uid}"; # User-id must match above user. MPD will look inside this directory for the PipeWire socket.
+    XDG_RUNTIME_DIR = "/run/user/1000"; # User-id must match above user. MPD will look inside this directory for the PipeWire socket.
   };
 }
