570f3d511a46fc97e0a2e76dbe63eeb91ad773a5
Ahwx <ahwx@ahwx.org>
2025-07-26 19:06:39 +0200

    chore: cleanup; hard-disable avahi since it's not required

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -1,20 +1,13 @@
-{ pkgs, ... }: 
+{ pkgs, ... }:
 {
   networking = {
     networkmanager.enable = true;
     nameservers = [ "9.9.9.9" ];
     firewall = {
       enable = true;
-      # allowedTCPPorts = [ 22 80 443 59010 59011 ];
-      # allowedUDPPorts = [ 59010 59011 ];
-      # allowedUDPPortRanges = [
-        # { from = 4000; to = 4007; }
-        # { from = 8000; to = 8010; }
-      # ];
     };
   };
-
-  # environment.systemPackages = with pkgs; [
-  #   networkmanagerapplet
-  # ];
+  services = {
+    avahi.enable = false;
+  };
 }
