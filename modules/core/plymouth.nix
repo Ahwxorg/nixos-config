825b6ecced905b7b9ced007c0f1d8f4f7d747823
Ahwx <ahwx@ahwx.org>
2025-07-27 00:41:43 +0200

    feat: actually set theme; also enable silent boot

--- modules/core/plymouth.nix
+++ modules/core/plymouth.nix
@@ -5,19 +5,36 @@
 }:
 {
   # TODO: add https://github.com/FraioVeio/plymouth-xp-theme
-  boot.loader.timeout = 1;
-  boot.plymouth = {
-    enable = lib.mkDefault true;
-    themePackages = with pkgs; [
-      # By default we would install all themes
-      (adi1090x-plymouth-themes.override {
-        selected_themes = [ "lone" ];
-        # selected_themes = [ "sliced" ];
-        # selected_themes = [ "rings" ];
-        # selected_themes = [ "red_loader" ];
-        # selected_themes = [ "dna" ];
-        # selected_themes = [ "hexagon_dots" ];
-      })
+  boot = {
+    plymouth = {
+      enable = lib.mkDefault true;
+      theme = "lone";
+      themePackages = with pkgs; [
+        # By default we would install all themes
+        (adi1090x-plymouth-themes.override {
+          selected_themes = [ "lone" ];
+          # selected_themes = [ "sliced" ];
+          # selected_themes = [ "rings" ];
+          # selected_themes = [ "red_loader" ];
+          # selected_themes = [ "dna" ];
+          # selected_themes = [ "hexagon_dots" ];
+        })
+      ];
+    };
+
+    # Enable "Silent boot"
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "udev.log_priority=3"
+      "rd.systemd.show_status=auto"
     ];
+    # Hide the OS choice for bootloaders.
+    # It's still possible to open the bootloader list by pressing any key
+    # It will just not appear on screen unless a key is pressed
+    loader.timeout = 1;
   };
 }
