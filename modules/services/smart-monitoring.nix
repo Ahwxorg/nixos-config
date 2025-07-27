3401e1d05a7d9e8beca230018b6333fbe96234fd
Ahwx <ahwx@ahwx.org>
2025-07-28 00:57:04 +0200

    feat: enable `scrutiny` on dandelion, disable otherwise; enable `smartd` with system mailer

--- modules/services/smart-monitoring.nix
+++ modules/services/smart-monitoring.nix
@@ -1,7 +1,15 @@
-{ config, ... }:
+{ config, host, ... }:
 {
+  imports = [ ./email.nix ];
   services.scrutiny = {
-    enable = true;
+    # Enable based on name of host
+    enable =
+      if (host == "dandelion") then
+        true
+      else if (host == "lily") then
+        true
+      else
+        false;
     collector.enable = true;
     settings.web.listen.port = 8181;
     settings.notify.urls = [
@@ -10,18 +18,20 @@
     ];
   };
 
-  # services.smartd = {
-  #   enable = true;
-  #   autodetect = true;
-  #   notifications = {
-  #     mail = {
-  #       enable = true;
-  #       # mailer = "/path/to/mailer/binary"; # Need to get system emails working first
-  #       sender = "${config.liv.variables.fromEmail}";
-  #       recipient = "${config.liv.variables.toEmail}";
-  #     };
-  #   };
-  # };
+  services.smartd = {
+    enable = true;
+    autodetect = true;
+    notifications = {
+      wall = {
+        enable = true;
+      };
+      mail = {
+        enable = true;
+        sender = config.liv.variables.senderEmail;
+        recipient = config.liv.variables.email;
+      };
+    };
+  };
 
   # services.nginx.virtualHosts."" = {
   #   locations."/" = {
