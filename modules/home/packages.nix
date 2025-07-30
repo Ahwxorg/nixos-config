962f39659683e4f16fa07d6e7b0b38b3115e37ef
Ahwx <ahwx@ahwx.org>
2025-07-30 16:33:52 +0200

    feat: write cursed package function

--- modules/home/packages.nix
+++ modules/home/packages.nix
@@ -1,107 +1,126 @@
-{ inputs, pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    # Environment shit
-    tesseract
-    yubikey-touch-detector
-    wireguard-tools
-    openresolv
-    xdg-utils
-    killall
-    libnotify
-    openssl
-    pamixer
-    playerctl
-    wl-clipboard
-    cliphist
-    poweralertd
-    ffmpeg
-    zip
-    unzip
-    wget
-    xxd
-    gcc
-    gnumake
-    python3
+  inputs,
+  lib,
+  pkgs,
+  ...
+}:
+with lib;
+let
+  guiPkgs =
+    if (config.liv.gui == true) then
+      [
+        element-desktop
+        gajim
+        signal-desktop
+        anki-bin
+        obs-studio
+        wdisplays
+        librewolf # main
+        ungoogled-chromium # for things that don't work with librewolf
+        nsxiv
+        imv
+        libreoffice
+        xfce.thunar
+        spotify
+        spotify-player
+        thunderbird
+        lxqt.pavucontrol-qt
+        mpv
+        plasma5Packages.kdeconnect-kde
+        # onthespot-overlay
 
-    # CLI shit
-    termpdfpy
-    vimv
-    iamb
-    pass
-    lm_sensors
-    neofetch
-    hyfetch
-    glow
-    eva
-    exiftool
-    translate-shell
-    progress
-    pwgen
-    jq
-    tmux
-    htop
-    eza
-    file
-    fzf
-    lazygit
-    gitleaks
-    ripgrep
-    yt-dlp
-    spotify-player
-    nodejs_22
-    yarn
-    cargo
-    rustc
-    wikit
-    reader
-    nmap
-    speedtest-go
-    delta
-    powertop
-    android-tools
-    sshpass
+        # Gaming
+        lunar-client
+      ]
+    else
+      [
+        killall
+      ];
+in
+{
+  home.packages =
+    with pkgs;
+    [
+      # Environment shit
+      tesseract
+      yubikey-touch-detector
+      wireguard-tools
+      openresolv
+      xdg-utils
+      killall
+      libnotify
+      openssl
+      pamixer
+      playerctl
+      wl-clipboard
+      cliphist
+      poweralertd
+      ffmpeg
+      zip
+      unzip
+      wget
+      xxd
+      gcc
+      gnumake
+      python3
 
-    # Install pip packages
-    # python3
-    # python3Packages.pip
-    # (writeShellScriptBin "install-pip-packages" '' # This script does not run, yet.
-    #   pip install --user --break-system-packages <package>
-    # '')
+      # CLI shit
+      termpdfpy
+      vimv
+      iamb
+      pass
+      lm_sensors
+      neofetch
+      hyfetch
+      glow
+      eva
+      exiftool
+      translate-shell
+      progress
+      pwgen
+      jq
+      tmux
+      htop
+      eza
+      file
+      fzf
+      lazygit
+      gitleaks
+      ripgrep
+      yt-dlp
+      nodejs_22
+      yarn
+      cargo
+      rustc
+      wikit
+      reader
+      nmap
+      speedtest-go
+      delta
+      powertop
+      android-tools
+      sshpass
+      net-tools
+      nmap
 
-    # GUI shit
-    element-desktop
-    gajim
-    signal-desktop
-    anki-bin
-    obs-studio
-    wdisplays
-    librewolf # main
-    ungoogled-chromium # for things that don't work with librewolf
-    nsxiv
-    imv
-    libreoffice
-    xfce.thunar
-    spotify
-    thunderbird
-    lxqt.pavucontrol-qt
-    mpv
-    plasma5Packages.kdeconnect-kde
-    # onthespot-overlay
+      # Install pip packages
+      # python3
+      # python3Packages.pip
+      # (writeShellScriptBin "install-pip-packages" '' # This script does not run, yet.
+      #   pip install --user --break-system-packages <package>
+      # '')
 
-    # Gaming
-    lunar-client
+      inputs.alejandra.defaultPackage.${system}
+      inputs.nixvim.packages.${pkgs.system}.default
+      mermaid-cli
+      gnuplot
 
-    inputs.alejandra.defaultPackage.${system}
-    inputs.nixvim.packages.${pkgs.system}.default
-    mermaid-cli
-    gnuplot
-
-    # Email/calendar/etc
-    neomutt
-    w3m
-    khard
-    khal
-    vdirsyncer
-  ];
+      # Email/calendar/etc
+      neomutt
+      w3m
+      khard
+      khal
+      vdirsyncer
+    ]
+    ++ guiPkgs;
 }
