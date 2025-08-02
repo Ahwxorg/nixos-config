e110052eea93f3634b98219671e858f3e806b7c9
Ahwx <ahwx@ahwx.org>
2025-08-02 12:35:38 +0200

    chore: comment out bootloader as that's not required apparently??

--- hosts/hazel/default.nix
+++ hosts/hazel/default.nix
@@ -39,10 +39,11 @@
     ];
   };
 
-  boot = {
-    loader.grub = {
-      enable = true;
-      device = "/dev/sda";
-    };
-  };
+  #boot = {
+  #  loader.grub = {
+  #    enable = true;
+  #    device = "/dev/sda";
+  #    useOSProber = true;
+  #  };
+  #};
 }
