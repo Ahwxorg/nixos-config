29521f8994aa2b5669583c42bf5411f9ee808964
Ahwx <ahwx@ahwx.org>
2025-08-05 21:02:01 +0200

    chore: import `syncthing` on `dandelion`

--- modules/services/dandelion.nix
+++ modules/services/dandelion.nix
@@ -7,6 +7,7 @@
     ++ [ (import ./home-assistant.nix) ]
     ++ [ (import ./monitoring.nix) ]
     ++ [ (import ./smart-monitoring.nix) ]
+    ++ [ (import ./syncthing.nix) ]
     ++ [ (import ./tailscale.nix) ]
     ++ [ (import ./nfs.nix) ]
     ++ [ (import ./hd-idle.nix) ];
