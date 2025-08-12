e9c413994f4117c1e8f8696955dbacc3f18922d6
Ahwx <ahwx@ahwx.org>
2025-08-12 16:18:16 +0200

    feat: add `createScript` wrapper also stolen from @ivarwithoutbones

---
+++ pkgs/createScript/default.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  runCommand,
+  makeWrapper,
+}:
+
+# A function which creates a shell script with optional dependencies added to PATH.
+
+name: src:
+{
+  dependencies ? [ ],
+  ...
+}@attrs:
+
+runCommand name
+  (
+    {
+      inherit src;
+      nativeBuildInputs = lib.optionals (dependencies != [ ]) (attrs.nativeBuildInputs or [ ]) ++ [
+        makeWrapper
+      ];
+
+      meta = {
+        mainProgram = name;
+      } // attrs.meta or { };
+    }
+    // (builtins.removeAttrs attrs [
+      "nativeBuildInputs"
+      "meta"
+    ])
+  )
+  ''
+    mkdir -p $out/bin
+    install -Dm755 $src $out/bin/$name
+    patchShebangs $out/bin/$name
+
+    ${lib.optionalString (dependencies != [ ]) ''
+      wrapProgram $out/bin/$name --prefix PATH : ${lib.makeBinPath dependencies}
+    ''}
+  ''
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,5 @@
 { pkgs }:
 {
-  # wikit = pkgs.callPackage ./wikit/default.nix { };
+  createScript = pkgs.callPackage ./createScript/default.nix { };
   nix-search-fzf = pkgs.callPackage ./nix-search-fzf/default.nix { };
 }
