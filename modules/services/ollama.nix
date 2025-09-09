b4d41e5e66d4b03751b66dddd96df7206e03c5c9
Ahwx <ahwx@ahwx.org>
2025-09-09 16:05:12 +0200

    feat: adds ollama

---
+++ modules/services/ollama.nix
@@ -0,0 +1,9 @@
+{
+  services.ollama = {
+    enable = true;
+    # Optional: preload models, see https://ollama.com/library
+    # loadModels = [ "llama3.2:3b" "deepseek-r1:1.5b"];
+    # acceleration = "rocm"; # nope, 5700XT is too old for this
+  };
+  services.open-webui.enable = true;
+}
