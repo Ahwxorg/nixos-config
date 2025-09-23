9b7fbb34710e3e4b7e7f98a60cc1c660d7976c17
Ahwx <ahwx@ahwx.org>
2025-09-23 13:02:00 +0200

    chore: adds more shares

--- modules/services/nfs.nix
+++ modules/services/nfs.nix
@@ -9,13 +9,23 @@
     # Network shares
     samba = {
       package = pkgs.samba;
-      # ^^ `samba4Full` is compiled with avahi, ldap, AD etc support (compared to the default package, `samba`
+      # ^^ `samba4Full` is compiled with avahi, ldap, AD etc support (compared to the default package, `samba`. samba4Full gives issue, however)
       # Required for samba to register mDNS records for auto discovery
       # See https://github.com/NixOS/nixpkgs/blob/592047fc9e4f7b74a4dc85d1b9f5243dfe4899e3/pkgs/top-level/all-packages.nix#L27268
       enable = true;
       openFirewall = true;
       shares.main = {
         path = "/spinners/rootvol/nfs";
+        writable = "true";
+        comment = "Hello world!";
+      };
+      shares.violet = {
+        path = "/spinners/violet";
+        writable = "true";
+        comment = "Hello world!";
+      };
+      shares.ahwx = {
+        path = "/spinners/ahwx";
         writable = "true";
         comment = "Hello world!";
       };
