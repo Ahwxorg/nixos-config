414459ca837e3edd12eaace5133008159e783adc
Ahwx <ahwx@ahwx.org>
2025-10-21 12:12:18 +0200

    chore: adds uptime-kuma

---
+++ modules/services/uptime-kuma.nix
@@ -0,0 +1,25 @@
+{ config, ... }:
+{
+  services = {
+    uptime-kuma = {
+      enable = true;
+      settings.PORT = 4800;
+    };
+    anubis.instances.uptime-kuma = {
+      settings = {
+        TARGET = "http://localhost:4800";
+        BIND = ":4801";
+        BIND_NETWORK = "tcp";
+      };
+    };
+    nginx.virtualHosts."uptime.liv.town" = {
+      forceSSL = true;
+      sslCertificate = "/var/lib/acme/liv.town/cert.pem";
+      sslCertificateKey = "/var/lib/acme/liv.town/key.pem";
+      locations."/" = {
+        proxyPass = "http://localhost${toString config.services.anubis.instances.uptime-kuma.settings.BIND}";
+        proxyWebsockets = true;
+      };
+    };
+  };
+}
