5fe53697d477aa34f4a6912cb923a8464fb4f221
Ahwx <ahwx@ahwx.org>
2025-11-15 20:51:14 +0100

    fix: ollama things

--- modules/services/ollama.nix
+++ modules/services/ollama.nix
@@ -3,7 +3,7 @@
     enable = true;
     # Optional: preload models, see https://ollama.com/library
     # loadModels = [ "llama3.2:3b" "deepseek-r1:1.5b"];
-    # acceleration = "rocm"; # nope, 5700XT is too old for this
+    acceleration = "rocm"; # nope, 5700XT is too old for this
   };
-  services.open-webui.enable = false;
+  services.open-webui.enable = true;
 }
