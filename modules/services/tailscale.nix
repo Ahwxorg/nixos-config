00efec076a390c963e4152e1bdc0555fe9713a48
Ahwx <ahwx@ahwx.org>
2025-11-21 11:39:16 +0100

    feat: adds some `tailscale` options to make it less annoying

--- modules/services/tailscale.nix
+++ modules/services/tailscale.nix
@@ -1,3 +1,10 @@
 {
-  services.tailscale.enable = true;
+  services.tailscale = {
+    enable = true;
+    useRoutingFeatures = "client";
+    extraUpFlags = [
+      # "--accept-dns=false"
+      "--accept-routes"
+    ];
+  };
 }
