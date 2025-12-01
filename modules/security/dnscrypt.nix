8657c126097803468e04e4170a1f4f26bc313b2a
Ahwx <ahwx@ahwx.org>
2025-12-01 14:32:36 +0100

    chore: update `minisign_key`; should automate this somehow?

--- modules/security/dnscrypt.nix
+++ modules/security/dnscrypt.nix
@@ -25,7 +25,7 @@ in
           "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
           "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
         ];
-        minisign_key = "sha256-LRw8acNa39MUv7XmA0GuheugECcejbN8+GJl2Ra7lpg="; # See https://github.com/DNSCrypt/dnscrypt-resolvers/blob/master/v3/public-resolvers.md
+        minisign_key = "sha256-cM9AYkf1ORyDmwN4oE4CNG8sepdzQi3XT968SDsOtCY="; # See https://github.com/DNSCrypt/dnscrypt-resolvers/blob/master/v3/public-resolvers.md
         cache_file = "/var/lib/dnscrypt/public-resolvers.md";
       };
 
