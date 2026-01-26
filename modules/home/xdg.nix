17f3a7f07f97a6c2782a08d6519d2dac12c42170
Ahwx <ahwx@ahwx.org>
2026-01-26 22:39:30 +0100

    feat: adds move `xdg` defaults

--- modules/home/xdg.nix
+++ modules/home/xdg.nix
@@ -2,8 +2,40 @@
   xdg.mimeApps = {
     enable = true;
     defaultApplications = {
+      "image/tiff" = "nsxiv.desktop";
+      "image/jp2" = "nsxiv.desktop";
+      "image/jxl" = "nsxiv.desktop";
+      "image/jpeg" = "nsxiv.desktop";
+      "image/x-canon-cr2" = "nsxiv.desktop";
+      "image/x-jp2-codestream" = "nsxiv.desktop";
+      "image/webp" = "nsxiv.desktop";
+      "image/x-nikon-nef" = "nsxiv.desktop";
+      "image/x-sony-arw" = "nsxiv.desktop";
+      "image/gif" = "nsxiv.desktop";
+      "image/x-fuji-raf" = "nsxiv.desktop";
+      "image/heif" = "nsxiv.desktop";
+      "audio/mpeg" = "mpv.desktop";
+      "audio/mp3" = "mpv.desktop";
+      "audio/vnd.wave" = "mpv.desktop";
+      "video/mp4" = "mpv.desktop";
+      "video/mkv" = "mpv.desktop";
       "x-scheme-handler/sgnl" = "signal-desktop.desktop";
       "x-scheme-handler/signalcaptcha" = "signal-desktop.desktop";
+      "x-scheme-handler/http" = "chromium-browser.desktop";
+      "x-scheme-handler/https" = "chromium-browser.desktop";
+      "x-scheme-handler/about" = "chromium-browser.desktop";
+      "x-scheme-handler/unknown" = "chromium-browser.desktop";
+      "x-scheme-handler/mailto" = "thunderbird.desktop";
+      "x-scheme-handler/chrome" = "thunderbird.desktop";
+      "text/calendar" = "thunderbird.desktop";
+      "application/pdf" = "org.pwmt.zathura-pdf-mupdf.desktop";
+      "application/x-extension-htm" = "chromium-browser.desktop";
+      "application/x-extension-html" = "chromium-browser.desktop";
+      "application/x-extension-shtml" = "chromium-browser.desktop";
+      "application/x-extension-xht" = "chromium-browser.desktop";
+      "application/x-extension-xhtml" = "chromium-browser.desktop";
+      "application/x-zerosize" = "nsxiv.desktop";
+      "application/xhtml+xml" = "chromium-browser.desktop";
     };
   };
 }
