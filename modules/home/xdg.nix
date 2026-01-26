a2ae9bfa4b72c1cff9a4d495ac5247fc802777b6
Ahwx <ahwx@ahwx.org>
2026-01-26 21:01:35 +0100

    feat: write first little bit of `xdg.mimeApps` options

---
+++ modules/home/xdg.nix
@@ -0,0 +1,9 @@
+{
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "x-scheme-handler/sgnl" = "signal-desktop.desktop";
+      "x-scheme-handler/signalcaptcha" = "signal-desktop.desktop";
+    };
+  };
+}
