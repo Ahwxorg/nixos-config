af37ea8f2d4ce5438b287f9ff775aaeb40728257
ahwx <liv@liv.town>
2026-02-13 12:56:28 +0100

    feat: add more items for `azalea`

--- modules/home/default.azalea.nix
+++ modules/home/default.azalea.nix
@@ -2,6 +2,7 @@
 {
   imports =
     [ (import ./git.nix) ]
+    ++ [ (import ./fonts.nix) ]
     ++ [ (import ./htop.nix) ]
     ++ [ (import ./kitty.nix) ]
     ++ [ (import ./nvim.nix) ]
@@ -9,6 +10,9 @@
     ++ [ (import ./packages.nix) ]
     ++ [ (import ./scripts/scripts.nix) ]
     ++ [ (import ./ssh.nix) ]
+    ++ [ (import ./skhd.nix) ]
+    ++ [ (import ./sketchybar/default.nix) ]
+    ++ [ (import ./jankyborders.nix) ]
     ++ [ (import ./zsh.nix) ]
     ++ [ (import ./qutebrowser.nix) ]
     ++ [ (import ./tmux.nix) ];
