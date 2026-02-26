12b7809c049c827f6626a3c3add32255a4764c0c
ahwx <liv@liv.town>
2026-02-27 00:54:23 +0100

    feat: adds background blur and hides macos windows decorations

--- modules/home/kitty.nix
+++ modules/home/kitty.nix
@@ -11,6 +11,8 @@
     };
 
     settings = {
+      hide_window_decorations = "yes";
+      background_blur = "16";
       confirm_os_window_close = 0;
       background_opacity = "0.75";
       window_padding_width = 10;
