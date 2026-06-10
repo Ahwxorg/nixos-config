f177d6e7d472e28863599800571857fe7e40aa44
Ahwx <ahwx@ahwx.org>
2026-06-10 20:54:22 +0200

    feat: pin new base version for docker

--- modules/services/docker.nix
+++ modules/services/docker.nix
@@ -1,11 +1,17 @@
-{ username, ... }:
+{
+  username,
+  pkgs,
+  lib,
+  ...
+}:
 {
   virtualisation.docker = {
+    package = lib.mkDefault pkgs.docker_29;
     enable = true;
-    autoPrune.enable = true;
+    # autoPrune.enable = true;
     # enableNvidia = true;
   };
-  
+
   users.users.${username} = {
     extraGroups = [ "docker" ];
   };
