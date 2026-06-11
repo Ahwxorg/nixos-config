7468b20c8874f7bd0179e7748d0b93b65246811a
Ahwx <ahwx@ahwx.org>
2026-06-12 01:58:59 +0200

    feat: adds back autoPrune to docker, as it was not causing any issues at all actually

--- modules/core/docker.nix
+++ modules/core/docker.nix
@@ -8,7 +8,7 @@
   virtualisation.docker = {
     package = lib.mkDefault pkgs.docker_29;
     enable = true;
-    # autoPrune.enable = true;
+    autoPrune.enable = true;
     # enableNvidia = true;
   };
 
