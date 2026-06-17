42b7de5dd7945301a85c682e41a029089a17914f
Ahwx <ahwx@ahwx.org>
2026-06-17 13:01:13 +0200

    chore: enable gnome-keyring and set extraPortals to be hyprland's portal

--- modules/core/wayland.nix
+++ modules/core/wayland.nix
@@ -9,13 +9,15 @@
     };
   };
 
+  services.gnome.gnome-keyring.enable = true;
+
   xdg.portal = {
     configPackages = [ pkgs.xdg-desktop-portal-gtk ];
     enable = true;
     wlr.enable = true;
     xdgOpenUsePortal = true;
-    # extraPortals = [
-    #   pkgs.xdg-desktop-portal-hyprland
-    # ];
+    extraPortals = [
+      pkgs.xdg-desktop-portal-hyprland
+    ];
   };
 }
