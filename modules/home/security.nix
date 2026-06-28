8d84d8800ce7b9ae3513db1e55882107420a4038
Ahwx <ahwx@ahwx.org>
2026-06-28 11:14:21 +0200

    feat: install keybase, import own public key, enable zsh integration

--- modules/home/security.nix
+++ modules/home/security.nix
@@ -2,11 +2,23 @@
   services.gpg-agent = {
     enable = true;
     enableSshSupport = true;
+    enableZshIntegration = true;
   };
+
+  services.keybase.enable = true;
 
   programs.gpg = {
     enable = true;
     mutableKeys = false;
     mutableTrust = false;
+    publicKeys = [
+      {
+        source = builtins.fetchurl {
+          url = "https://keybase.io/livtown/pgp_keys.asc?fingerprint=2c565233f609450e109249c4b7e0db563fd1f754";
+          sha256 = "sha256-xsR6Mm5OqVLBOJB89939t7Db2Y/wBzu4Raad6O174QM=";
+        };
+        trust = "ultimate";
+      }
+    ];
   };
 }
