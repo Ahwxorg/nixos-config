5f2d6ac17d671f824102e92585ad998aa30fd8fc
Ahwx <ahwx@ahwx.org>
2026-07-19 15:30:01 +0200

    chore: cleanup old devices that are no longer used/have been replaced by a security key

--- modules/core/sshd.nix
+++ modules/core/sshd.nix
@@ -19,14 +19,10 @@
   networking.firewall.allowedTCPPorts = config.services.openssh.ports;
 
   users.users.${username}.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGXi00z/rxVrWLKgYr+tWIsbHsSQO75hUMSTThNm5wUw liv@sakura" # sakura
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHv2zxCy22KU1tZOH2hA1p8fWVpOSrTYF68+3E5r330O liv@ichiyo" # ichiyo
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKQtG69zrMFsoHForwZEi66y1tPvctqg1OgjQFrF3OI+ liv@iris" # iris
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK2ENAFTKtYn3cT9x8l+4BJkrvg1ZTF2LTFOkONTz1OD liv@iris" # iris@windows
-    # "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINKI2KQn97mykFLIaMUWMftA1txJec9qW56hAMj5/MhE liv@dandelion" # dandelion
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILwDS8tXjGjUtk3eQAaPf0S0f9JgwEGPlNYQ7OvACX1Z liv@imilia" # imilia
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN7Qlz0vKb8EtqiyRRz1PLmcWR9mxq39BaAcUU4Ls2pM liv@april" # april
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAR49vTfzwC2cd09nE2zAsi5GvjLKyYSXsi072hwmMV2 liv@azalea" # azalea
+    # "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAR49vTfzwC2cd09nE2zAsi5GvjLKyYSXsi072hwmMV2 liv@azalea" # azalea
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOh90Kba/KNwWafa26+eBpNFer4qQ0KgF35Q+9gteyo/ liv@clover" # clover
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAD8DIqgrOXuI/EeOD7tSfpDikJfj9CmoghemkwkL0Dh openpgp:0x04CCA7CF" # yubi
   ];
 }
