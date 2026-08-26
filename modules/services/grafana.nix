eed200d1aa33ce56d5dcd50897b340eefc58476b
Ahwx <ahwx@ahwx.org>
2026-08-26 16:57:27 +0200

    feat: write new grafana config

--- modules/services/grafana.nix
+++ modules/services/grafana.nix
@@ -5,17 +5,17 @@
   pkgs,
   ...
 }:
-let
-  baseRepo = "ssh://liv@dandelion:9123/spinners/rootvol/backups/${host}";
-in
 {
   services = {
     grafana = {
       enable = true;
-      settings.server = {
-        domain = "monitoring.liv.town";
-        http_addr = "127.0.0.1";
-        http_port = 2342;
+      settings = {
+        server = {
+          domain = "monitoring.liv.town";
+          http_addr = "127.0.0.1";
+          http_port = 2342;
+        };
+        security.secret_key = "$__file{${config.sops.secrets.grafanaKeyFile.path}}";
       };
     };
 
@@ -28,30 +28,5 @@ in
         proxyWebsockets = true;
       };
     };
-    # borgbackup.jobs."violet-grafana" = {
-    #   paths = [ "/var/lib/grafana" ];
-    #   repo = "${baseRepo}/var-grafana";
-    #   encryption.mode = "none";
-    #   compression = "auto,zstd";
-    #   startAt = "daily";
-    #   preHook = ''
-    #     systemctl stop grafana
-    #   '';
-    #   postHook = ''
-    #     systemctl start grafana
-    #     if [ $exitStatus -eq 2 ]; then
-    #       ${pkgs.ntfy-sh}/bin/ntfy send https://notify.liv.town/${host} "borgbackup: ${host} backup (grafana) failed with errors"
-    #     else
-    #       ${pkgs.ntfy-sh}/bin/ntfy send https://notify.liv.town/${host} "borgbackup: ${host} backup (grafana) completed succesfully with exit status $exitStatus"
-    #     fi
-    #   '';
-    #   user = "root";
-    #   extraCreateArgs = [
-    #     "--stats"
-    #   ];
-    #   environment = {
-    #     BORG_RSH = "ssh -p 9123 -i /home/${username}/.ssh/id_ed25519";
-    #   };
-    # };
   };
 }
