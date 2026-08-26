fe041ec78d7c6b811f909c21e5e44eef72fc6569
Ahwx <ahwx@ahwx.org>
2026-08-26 16:57:39 +0200

    chore: set other volume because starting from scratch and docker didn't like me

--- modules/services/home-assistant.nix
+++ modules/services/home-assistant.nix
@@ -3,7 +3,7 @@
   virtualisation.oci-containers = {
     backend = "docker";
     containers.homeassistant = {
-      volumes = [ "home-assistant:/config" ];
+      volumes = [ "homeassistant:/config" ];
       environment.TZ = "Europe/Amsterdam";
       image = "ghcr.io/home-assistant/home-assistant:stable"; # Warning: if the tag does not change, the image will not be updated
       extraOptions = [
