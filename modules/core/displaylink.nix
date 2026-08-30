3a83085358fe75281d7430a0e437abd3467e5b03
Ahwx <ahwx@ahwx.org>
2026-08-30 13:58:33 +0200

    feat: displaylink works now, i think?

--- modules/core/displaylink.nix
+++ modules/core/displaylink.nix
@@ -1,4 +1,24 @@
-{ ... }: {
-  services.xserver.videoDrivers = [ "displaylink" "modesetting" ];
+{ config, pkgs, ... }:
+{
+  environment.systemPackages = [
+    pkgs.displaylink
+    # pkgs.linuxPackages.evdi
+  ];
+
+  boot = {
+    kernelModules = [ "evdi" ];
+    initrd.kernelModules = [ "evdi" ];
+
+    extraModulePackages = [
+      config.boot.kernelPackages.evdi
+    ];
+  };
+
+  services.udev.packages = [ pkgs.displaylink ];
+
+  services.xserver.videoDrivers = [
+    "displaylink"
+    "modesetting"
+  ];
   systemd.services.dlm.wantedBy = [ "multi-user.target" ];
 }
