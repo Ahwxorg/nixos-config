6c7203bcb634550ad458ebbcc9156853da644b5e
Ahwx <ahwx@ahwx.org>
2026-08-30 14:04:09 +0200

    feat: set tofi theme

---
+++ modules/home/tofi.nix
@@ -0,0 +1,25 @@
+{
+  programs.tofi = {
+    enable = true;
+    settings = {
+      font = "scientifica";
+      font-size = 24;
+
+      outline-width = 0;
+      border-width = 0;
+      padding-left = "4%";
+      padding-top = "2%";
+      padding-right = 0;
+      padding-bottom = 0;
+
+      background-color = "#111";
+      text-color = "#f8f8f8";
+      selection-color = "#cda5ef";
+
+      width = "100%";
+      height = "100%";
+
+      hide-cursor = true;
+    };
+  };
+}
