c4e80096032667ec289333f409af583a5c90fa84
Ahwx <ahwx@ahwx.org>
2026-08-30 13:59:11 +0200

    feat: theme bemenu

---
+++ modules/home/bemenu.nix
@@ -0,0 +1,24 @@
+{
+  programs.bemenu = {
+    enable = true;
+    settings = {
+      # center = false;
+      ignorecase = true;
+      list = 500;
+      prompt = "    ★ ";
+      fn = "scientifica";
+      tb = "#161616";
+      tf = "#f8f8f8";
+      fb = "#161616";
+      ff = "#f8f8f8";
+      cb = "#161616";
+      cf = "#f8f8f8";
+      nb = "#161616";
+      nf = "#f8f8f8";
+      hb = "#121212";
+      hf = "#cda5ef";
+      scb = "#f8f8f8";
+      # width-factor = 0.3;
+    };
+  };
+}
