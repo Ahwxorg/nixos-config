f1c259321b8fb2b4d6f1c244680b63ab1997c637
Ahwx <ahwx@ahwx.org>
2026-09-01 03:34:59 +0200

    chore: move to base16 colors for swaync; move to oxocarbon finally

--- modules/home/swaync/default.nix
+++ modules/home/swaync/default.nix
@@ -111,37 +111,25 @@
             }
       			'';
     file."/home/${username}/.config/swaync/style.css".text = ''
-      @define-color accent #f5c2e7;
-      @define-color secondary_accent #89b4fa;
-
-      @define-color rosewater #f5e0dc;
-      @define-color flamingo #f2cdcd;
-      @define-color mauve #cda5ef;
-      @define-color red #f38ba8;
-      @define-color maroon #eba0ac;
-      @define-color peach #fab387;
-      @define-color yellow #f9e2af;
-      @define-color green #a6e3a1;
-      @define-color teal #94e2d5;
-      @define-color sky #89dceb;
-      @define-color sapphire #74c7ec;
-      @define-color blue #89b4fa;
-      @define-color lavender #b4befe;
-      @define-color text #aaaaaa;
-      @define-color subtext1 #bac2de;
-      @define-color subtext0 #a6adc8;
-      @define-color overlay2 #9399b2;
-      @define-color overlay1 #7f849c;
-      @define-color overlay0 #6c7086;
-      @define-color surface2 #585b70;
-      @define-color surface1 #45475a;
-      @define-color surface0 #313244;
-      @define-color base #1e1e2e;
-      @define-color bg #000000;
-      @define-color crust #11111b;
+      @define-color base00 #161616; 
+      @define-color base01 #262626; 
+      @define-color base02 #393939; 
+      @define-color base03 #525252; 
+      @define-color base04 #dde1e6; 
+      @define-color base05 #f2f4f8; 
+      @define-color base06 #ffffff; 
+      @define-color base07 #08bdba;
+      @define-color base08 #3ddbd9; 
+      @define-color base09 #78a9ff; 
+      @define-color base0A #ee5396; 
+      @define-color base0B #33b1ff; 
+      @define-color base0C #ff7eb6; 
+      @define-color base0D #42be65; 
+      @define-color base0E #be95ff; 
+      @define-color base0F #82cfff;
 
       * {
-        color: @text;
+        color: @base05;
         all: unset;
         font-size: 16px;
         font-family: "SpaceMono Nerd Font";
@@ -149,14 +137,14 @@
       }
 
       .notification-row {
-        color: @subtext1;
+        color: @base04;
         outline: none;
         margin: 0;
         padding: 0px;
       }
 
       .floating-notifications.background .notification-row .notification-background {
-        background: @bg;
+        background: @base00;
         box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.6);
         border: 0px;
         border-radius: 3px;
@@ -170,14 +158,14 @@
         .notification {
         padding: 3px;
         border-radius: 3px;
-        border: 1px solid @surface2;
+        border: 1px solid @base04;
       }
 
       .floating-notifications.background
         .notification-row
         .notification-background
         .notification.critical {
-        border: 2px solid @red;
+        border: 2px solid @base08;
       }
 
       .floating-notifications.background
@@ -206,7 +194,7 @@
         .notification-action {
         all: unset;
         border-radius: 4px;
-        background-color: @bg-alt;
+        background-color: @base00;
         font-size: 5px;
         margin: 40px;
         font-weight: 200;
@@ -222,8 +210,8 @@
         > *:last-child
         > *
         .notification-action:hover {
-        background-color: @base;
-        border: 1px solid @crust;
+        background-color: @base00;
+        border: 1px solid @base01;
       }
 
       .floating-notifications.background
@@ -233,8 +221,8 @@
         > *:last-child
         > *
         .notification-action:active {
-        background-color: @crust;
-        color: @subtext1;
+        background-color: @base01;
+        color: @base04;
       }
 
       .image {
@@ -244,12 +232,12 @@
       .summary {
         font-weight: 800;
         font-size: 1rem;
-        color: @rosewater;
+        color: @base06;
       }
 
       .body {
         font-size: 0.8rem;
-        color: @subtext1;
+        color: @base04;
       }
 
       .floating-notifications.background
@@ -266,57 +254,29 @@
         .notification-row
         .notification-background
         .close-button:hover {
-        background-color: @base;
-        color: @subtext1;
+        background-color: @base00;
+        color: @base04;
       }
 
       .floating-notifications.background
         .notification-row
         .notification-background
         .close-button:active {
-        background-color: @bg;
-        color: @text;
+        background-color: @base00;
+        color: @base05;
       }
 
       .notification.critical progress {
-        background-color: @bg;
+        background-color: @base00;
       }
 
       .notification.low progress,
       .notification.normal progress {
-        background-color: @bg;
+        background-color: @base00;
       }
-      @define-color main_accent #cba6f7;
-      @define-color secondary_accent #89b4fa;
-
-      @define-color rosewater #f5e0dc;
-      @define-color flamingo #f2cdcd;
-      @define-color mauve #cba6f7;
-      @define-color red #f38ba8;
-      @define-color maroon #eba0ac;
-      @define-color peach #fab387;
-      @define-color yellow #f9e2af;
-      @define-color green #a6e3a1;
-      @define-color teal #94e2d5;
-      @define-color sky #89dceb;
-      @define-color sapphire #74c7ec;
-      @define-color blue #89b4fa;
-      @define-color lavender #b4befe;
-      @define-color text #cdd6f4;
-      @define-color subtext1 #bac2de;
-      @define-color subtext0 #a6adc8;
-      @define-color overlay2 #9399b2;
-      @define-color overlay1 #7f849c;
-      @define-color overlay0 #6c7086;
-      @define-color surface2 #585b70;
-      @define-color surface1 #45475a;
-      @define-color surface0 #313244;
-      @define-color base #000000;
-      @define-color bg #000000;
-      @define-color crust #11111b;
 
       * {
-        color: @text;
+        color: @base05;
         all: unset;
         font-size: 16px;
         font-family: "SpaceMono Nerd Font";
@@ -325,7 +285,7 @@
       }
 
       .control-center {
-        background: @bg;
+        background: @base00;
         border-radius: 3px;
         padding: 9px;
         margin: 0px;
@@ -337,35 +297,35 @@
       .widget-buttons-grid {
         margin: 0px;
         background: transparent;
-        color: @text;
+        color: @base05;
       }
 
       .widget-buttons-grid > flowbox > flowboxchild > button {
         margin: 3px;
         padding: 6px 12px;
-        background: @base;
-        color: @text;
+        background: @base00;
+        color: @base05;
         border-radius: 3px;
         border: 1px solid #45475a;
       }
 
       .widget-buttons-grid > flowbox > flowboxchild > button:hover {
-        background: @surface0;
+        background: @base02;
       }
 
       .widget-buttons-grid > flowbox > flowboxchild > button.toggle:checked {
-        background: @main_accent;
+        background: @base0C;
         border: 1px solid #313244;
       }
 
       .widget-buttons-grid > flowbox > flowboxchild > button.toggle:checked label {
-        color: @crust;
+        color: @base01;
       }
 
       /* notifications */
 
       .notification-row .notification-background {
-        background: @base;
+        background: @base00;
         padding: 0px;
         border-radius: 2px;
         margin: 5px 5px 5px 5px;
@@ -375,7 +335,7 @@
       .summary {
         font-size: 15px;
         background: transparent;
-        color: @text;
+        color: @base05;
         margin-left: 0px;
       }
 
@@ -383,7 +343,7 @@
         font-size: 11px;
         font-style: italic;
         background: transparent;
-        color: @subtext0;
+        color: @base04;
         text-shadow: none;
         margin: 3px 10px 0px 0px;
       }
@@ -398,7 +358,7 @@
         font-size: 13px;
         font-weight: normal;
         background: transparent;
-        color: @text;
+        color: @base05;
         text-shadow: none;
         margin-left: 0px;
       }
@@ -406,38 +366,38 @@
       .notification-group-headers {
         font-weight: bold;
         font-size: 11pt;
-        color: @mauve;
+        color: @base0E;
       }
 
       .notification-group-icon {
-        color: @green;
+        color: @base0B;
         margin-right: 8px;
       }
 
       .notification-group-collapse-button {
-        background: @maroon;
-        color: @base;
+        background: @base0A;
+        color: @base00;
         border: none;
         min-width: 24px;
         min-height: 24px;
       }
 
       .notification-group-collapse-button:hover {
-        background: @red;
-        color: @crust;
+        background: @base08;
+        color: @base01;
       }
 
       .notification-group-close-all-button {
-        background: @maroon;
-        color: @base;
+        background: @base0A;
+        color: @base00;
         border: none;
         min-width: 24px;
         min-height: 24px;
       }
 
       .notification-group-close-all-button:hover {
-        background: @red;
-        color: @crust;
+        background: @base08;
+        color: @base01;
       }
 
       .notification-default-action,
@@ -447,14 +407,14 @@
         box-shadow: none;
         background: transparent;
         border: none;
-        color: @text;
+        color: @base05;
         transition: all 0.15s ease-in-out;
         font-size: 11pt;
       }
 
       .notification-default-action:hover,
       .notification-action:hover {
-        background: @bg;
+        background: @base00;
       }
 
       .notification-default-action {
@@ -474,12 +434,12 @@
 
       .notification-action:first-child {
         border-bottom-left-radius: 10px;
-        background: @bg;
+        background: @base00;
       }
 
       .notification-action:last-child {
         border-bottom-right-radius: 10px;
-        background: @bg;
+        background: @base00;
       }
 
       .inline-reply {
@@ -487,47 +447,47 @@
       }
 
       .inline-reply-entry {
-        background: @crust;
-        color: @text;
-        caret-color: @mauve;
-        border: 1px solid @surface0;
+        background: @base01;
+        color: @base05;
+        caret-color: @base0E;
+        border: 1px solid @base02;
         border-radius: 10px;
       }
 
       .inline-reply-button {
         margin-left: 4px;
-        background: @crust;
-        border: 1px solid @surface0;
+        background: @base01;
+        border: 1px solid @base02;
         border-radius: 10px;
-        color: @text;
+        color: @base05;
       }
 
       .inline-reply-button:disabled {
         background: initial;
-        color: @overlay1;
+        color: @base03;
         border: 1px solid transparent;
       }
 
       .inline-reply-button:hover {
-        background: @surface1;
+        background: @base03;
       }
 
       /* Music player */
       .widget-mpris {
         /* background: alpha(@selected, 0.2); */
         border-radius: 3px;
-        color: @text;
+        color: @base05;
         padding: 6px;
         margin: 20px 6px;
       }
 
       .widget-mpris button {
-        color: alpha(@flamingo, 0.9);
+        color: alpha(@base0F, 0.9);
         border-radius: 4px;
       }
 
       .widget-mpris button:hover {
-        color: @text;
+        color: @base05;
       }
 
       .widget-mpris-player {
@@ -555,12 +515,12 @@
       trough {
         border-radius: 3px;
         min-height: 20px;
-        background: @surface2;
+        background: @base04;
       }
 
       trough highlight {
         padding: 4px;
-        background: @main_accent;
+        background: @base0C;
         border-radius: 3px;
       }
       trough slider {
@@ -571,11 +531,11 @@
       }
 
       .widget-volume {
-        background: @base;
+        background: @base00;
         padding: 2px 4px 2px 4px;
         margin: 4px 4px 1px 4px;
         border-radius: 2px;
-        color: @subtext0;
+        color: @base04;
         border: 1px solid #45475a;
       }
 
@@ -585,8 +545,8 @@
       }
 
       .widget-backlight {
-        background: @base;
-        color: @subtext0;
+        background: @base00;
+        color: @base04;
         padding: 2px 4px 2px 4px;
         margin: 1px 4px 4px 4px;
         border-radius: 2px;
