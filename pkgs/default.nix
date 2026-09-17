35bc4fbdadb13cc0ae48c24c311231e45566e2a1
Ahwx <ahwx@ahwx.org>
2026-09-17 14:42:45 +0200

    feat: adds `avd-fw` package

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,4 +3,5 @@
   createScript = pkgs.callPackage ./createScript/default.nix { };
   nix-search-fzf = pkgs.callPackage ./nix-search-fzf/default.nix { };
   nixos-centered-plymouth = pkgs.callPackage ./nixos-centered-plymouth/default.nix { };
+  avd-fw = pkgs.callPackage ./avd.nix { };
 }
